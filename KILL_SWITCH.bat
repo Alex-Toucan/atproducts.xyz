de945672e3d6612035d8c802be2e2ed5211a568f
Alex-Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2022-04-17 13:11:57 -0500

    Delete KILL_SWITCH.bat

--- KILL_SWITCH.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /F "tokens=4*" %%a in ('netsh interface show interface ^| more +2') do C:\Windows\System32\netsh.exe interface set interface name="%%a %%b" admin = disabled
-for /F "tokens=4*" %%a in ('netsh interface show interface ^| more +2') do C:\Windows\System32\netsh.exe interface set interface name="%%a%%b" admin = disabled
-
-reg add HKLM\SYSTEM\CurrentControlSet\Services\UsbStor /v "Start" /t REG_DWORD /d "4" /f
-
-echo Successfully blocked Internet connections and all USB drives.
-
-pause
