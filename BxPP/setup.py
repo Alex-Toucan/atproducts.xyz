f6ce84e047b73e8013e03421409a8cdd7aec5bd9
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-10-31 09:06:17 -0400

    Update setup.py

--- BxPP/setup.py
+++ BxPP/setup.py
@@ -7,9 +7,6 @@ INSTALL_DIR = '/usr/bin/'
 os.system(f'sudo git clone https://github.com/Discordmodsbers/BxPP {INSTALL_DIR}')
 os.system('sudo echo alias BxPP="python3 /usr/bin/BxPP/BxPP.py" >> /etc/skel/.bashrc ')
 os.system('sudo source /etc/skel/.bashrc ')
-os.system('sudo apt update -y')
-os.system('sudo apt-get upgrade -y')
 
 os.system('clear')
-print("Now all you have to run my interpeter is typing 'BxPP' with a file ofc.")
- 
+
