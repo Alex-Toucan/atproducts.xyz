a61c81dcd856b3e11ee166f9de3c581d0be486d7
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2022-12-21 14:39:17 -0600

    Delete info.js

--- static/info.js
+++
@@ -1,5 +0,0 @@
-document.write('
-          <b>Version:</b> 2.11<br>
-          <b>Author:</b> Alex Toucan<br><br>
-          <p>® <script src='/static/year.js'></scr" + "ipt> AT Products LLC</p>
-');
