c450610a7f08afc9fe9696c602845649a58537b7
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-01-12 11:30:17 -0600

    Update scroll.js

--- beta/scroll.js
+++ beta/scroll.js
@@ -1,15 +1,24 @@
-var rootElement = document.documentElement
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
 
 function handleScroll() {
   // Do something on scroll
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal ) > 0.80 ) {
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.8) {
     // Show button
-    scrollToTopBtn.classList.add("showBtn")
+    scrollToTopBtn.classList.add("showBtn");
   } else {
     // Hide button
-    scrollToTopBtn.classList.remove("showBtn")
+    scrollToTopBtn.classList.remove("showBtn");
   }
 }
 
-document.addEventListener("scroll", handleScroll)
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
