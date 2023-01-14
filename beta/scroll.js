c669d95b6a31c21ac55458a880e2c257ba085698
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-01-13 18:28:15 -0600

    Update scroll.js

--- beta/scroll.js
+++ beta/scroll.js
@@ -44,34 +44,6 @@ if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; re
         return false;
     });
 
-    // Team carousel
-    $(".team-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 30,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
