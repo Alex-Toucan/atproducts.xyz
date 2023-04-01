c5b9eba5b125d7e8aef69b26e876de63290ad05e
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-04-01 17:41:17 -0500

    Update 404.js

--- static/404.js
+++ static/404.js
@@ -2,8 +2,8 @@ function Previous() {
   window.history.back()
 }
 
-var 404id = document.getElementById("404btn");
+var 404id = document.getElementById('404btn');
 
-if (404id) {
+if(404id) {
   404id.addEventListener("click", Previous);
 }
