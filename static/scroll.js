b77c3ab0328981f2212b31d54b35bc3257a1ce6b
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-04-21 13:03:52 -0500

    Update scroll.js

--- static/scroll.js
+++ static/scroll.js
@@ -24,7 +24,7 @@ window.addEventListener("load", function (scrollload) {
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
-    });
-})(jQuery);
+    })(jQuery);
+});
   
 }
