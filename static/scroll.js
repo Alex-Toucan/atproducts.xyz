a6ba8c011de0ac61778cf6a4a924e25b5a5b8940
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-06-02 21:56:37 -0500

    Update scroll.js

--- static/scroll.js
+++ static/scroll.js
@@ -29,37 +29,3 @@ if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; re
 
 
 }
-
-// Set all carousel items to the same height
-    function carouselNormalization() {
-        
-        window.heights = [], //create empty array to store height values
-        window.tallest; //create variable to make note of the tallest slide
-        
-        function normalizeHeights() {
-            jQuery('.carousel-products .carousel-inner .carousel-item').each(function() { //add heights to array
-                window.heights.push(jQuery(this).outerHeight());
-            });
-            window.tallest = Math.max.apply(null, window.heights); //cache largest value
-            jQuery('.carousel-products .carousel-inner .carousel-item').each(function() {
-                jQuery(this).css('min-height',tallest + 'px');
-            });
-        }
-        normalizeHeights();
-
-        jQuery(window).on('resize orientationchange', function () {
-            
-            window.tallest = 0, window.heights.length = 0; //reset vars
-            jQuery('.carousel-products .carousel-inner .carousel-item').each(function() {
-                jQuery(this).css('min-height','0'); //reset min-height
-            }); 
-            
-            normalizeHeights(); //run it again 
-
-        });
-        
-    }
-    
-    jQuery( document ).ready(function() {
-        carouselNormalization();
-    });
