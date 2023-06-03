a84dbe5b260e178a1479496efa5f5f1e229973cc
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-06-02 23:00:40 -0500

    Update carousel.js

--- static/carousel.js
+++ static/carousel.js
@@ -5,11 +5,11 @@
         window.tallest; //create variable to make note of the tallest slide
         
         function normalizeHeights() {
-            jQuery('.carousel-products .carousel-inner .carousel-item').each(function() { //add heights to array
+            jQuery('#carousel-products .carousel-inner .carousel-item').each(function() { //add heights to array
                 window.heights.push(jQuery(this).outerHeight());
             });
             window.tallest = Math.max.apply(null, window.heights); //cache largest value
-            jQuery('.carousel-products .carousel-inner .carousel-item').each(function() {
+            jQuery('#carousel-products .carousel-inner .carousel-item').each(function() {
                 jQuery(this).css('min-height',tallest + 'px');
             });
         }
@@ -18,7 +18,7 @@
         jQuery(window).on('resize orientationchange', function () {
             
             window.tallest = 0, window.heights.length = 0; //reset vars
-            jQuery('.carousel-products .carousel-inner .carousel-item').each(function() {
+            jQuery('#carousel-products .carousel-inner .carousel-item').each(function() {
                 jQuery(this).css('min-height','0'); //reset min-height
             }); 
             
