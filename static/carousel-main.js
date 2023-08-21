9059f9ac47869e7cc85daf6b851d79951b5fdb50
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-08-20 19:27:15 -0500

    Update carousel-main.js

--- static/carousel-main.js
+++ static/carousel-main.js
@@ -1,12 +1,30 @@
-$(function () {
-    $('.carousel-main').carousel({
-        pause: "false"
-    });
+var playButton = document.querySelector(".playButton");
+var pauseButton = document.querySelector(".pauseButton");
+var nextButton = document.querySelector(".carousel-control-next");
+var previousButton = document.querySelector(".carousel-control-prev");
+var carouselMain = document.querySelector(".carousel-main");
+var slidebtn = document.querySelectorAll(".slidebtn");
 
-    $('.playButton').click(function () {
-        $('.carousel-main').carousel('cycle');
+$(function () {
+    $(carouselMain).carousel({
+        pause: "false",
+			  cycle: "true",
+			  interval: 10000
     });
-    $('.pauseButton').click(function () {
-        $('.carousel-main').carousel('pause');
+    
+    $(playButton).click(function () {
+        $(carouselMain).carousel('cycle');
     });
+    $(pauseButton).click(function () {
+        $(carouselMain).carousel('pause');
+    });
+	
+		$(nextButton).click(handleButton);
+    $(previousButton).click(handleButton);
+    $(slidebtn).click(handleButton);
+	
+    function handleButton() {
+        playButton.checked = true;
+			  pauseButton.checked = false;
+    };
 });
