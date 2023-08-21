1ce0104f858c519044d0bcfd2dbb505eaaa6e69f
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-08-20 19:25:16 -0500

    Update carousel-main.js

--- static/carousel-main.js
+++ static/carousel-main.js
@@ -1,28 +1,12 @@
-var playButton = document.querySelector(".playButton");
-var pauseButton = document.querySelector(".pauseButton");
-var nextButton = document.querySelector(".carousel-control-next");
-var previousButton = document.querySelector(".carousel-control-prev");
-var carouselMain = document.querySelector(".carousel-main");
-var slidebtn = document.querySelectorAll(".slidebtn");
-
 $(function () {
-    $(carouselMain).carousel({
-        pause: "false",
-			  cycle: "true",
-			  interval: 10000
+    $('.carousel-main').carousel({
+        pause: "false"
     });
-    
-    $(playButton).click(function () {
-        $(carouselMain).carousel('cycle');
+
+    $('.playButton').click(function () {
+        $('.carousel-main').carousel('cycle');
     });
-    $(pauseButton).click(function () {
-        $(carouselMain).carousel('pause');
+    $('.pauseButton').click(function () {
+        $('.carousel-main').carousel('pause');
     });
-		$(nextButton).click(handleButton);
-    $(previousButton).click(handleButton);
-    $(slidebtn).click(handleButton);
-    function handleButton() {
-        playButton.checked = true;
-			  pauseButton.checked = false;
-    };
 });
