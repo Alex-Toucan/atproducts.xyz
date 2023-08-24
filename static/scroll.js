dd2015cc88ec0b032bb52467293b7dfede6b32c1
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-08-23 20:37:09 -0500

    Update scroll.js

--- static/scroll.js
+++ static/scroll.js
@@ -1,35 +1,14 @@
 (function ($) {
     "use strict";
-    
-    // Check if the user prefers reduced motion
-    var prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
-
     $(window).scroll(function () {
-        // If user prefers reduced motion, instantly show or hide the button
-        if (prefersReducedMotion) {
-            if ($(this).scrollTop() > 100) {
-                $('.back-to-top').show();
-            } else {
-                $('.back-to-top').hide();
-            }
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
         } else {
-            // Otherwise, fade in/out the button as before
-            if ($(this).scrollTop() > 100) {
-                $('.back-to-top').fadeIn('slow');
-            } else {
-                $('.back-to-top').fadeOut('slow');
-            }
+            $('.back-to-top').fadeOut('slow');
         }
     });
-    
     $('.back-to-top').click(function () {
-        // If user prefers reduced motion, scroll instantly without animation
-        if (prefersReducedMotion) {
-            $('html, body').scrollTop(0);
-        } else {
-            // Otherwise, scroll smoothly with linear easing
-            $('html, body').animate({scrollTop: 0}, 1500, 'linear');
-        }
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
 
