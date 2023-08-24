78c1093f1b3049b229f44fe8ba6226bef6f75b86
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-08-23 20:36:16 -0500

    Update purecookie.js

--- static/purecookie.js
+++ static/purecookie.js
@@ -5,67 +5,71 @@ var purecookieLink = '<a href="/privacy#cookies" target="_blank" rel="noopener n
 var purecookieButton = "Understood"; // Button text
 // ---        --- //
 
-function pureFadeIn(elem, display) {
+
+function pureFadeIn(elem, display){
   var el = document.getElementById(elem);
   el.style.opacity = 0;
   el.style.display = display || "block";
 
-  if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-    el.style.opacity = 1; // If reduced motion, show element instantly without animation
-  } else {
-    (function fade() {
-      var val = parseFloat(el.style.opacity);
-      if (!((val += 0.02) > 1)) {
-        el.style.opacity = val;
-        requestAnimationFrame(fade);
-      }
-    })();
-  }
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .02) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
 };
-
-function pureFadeOut(elem) {
+	
+function pureFadeOut(elem){
   var el = document.getElementById(elem);
   el.style.opacity = 1;
 
-  if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-    el.style.display = "none"; // If reduced motion, hide element instantly without animation
-  } else {
-    (function fade() {
-      if ((el.style.opacity -= 0.02) < 0) {
-        el.style.display = "none";
-      } else {
-        requestAnimationFrame(fade);
-      }
+  (function fade() {
+    if ((el.style.opacity -= .02) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
       setTimeout(() => {
-        var theelement = document.querySelector('#cookieConsentContainer');
-        if (theelement) {
-          theelement.remove();
-        };
+      var theelement = document.querySelector('#cookieConsentContainer');
+     		if (theelement) {
+      		theelement.remove();
+      	};
       }, 1350);
-    })();
-  }
+  })();
 };
 
-function setCookie(name, value, days) {
-  // ... (your existing setCookie function)
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
-
 function getCookie(name) {
-  // ... (your existing getCookie function)
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
-
 function eraseCookie(name) {
-  // ... (your existing eraseCookie function)
+    document.cookie = name+'=; Max-Age=-99999999;';
 }
-
-window.addEventListener('load', function cookieConsent() { // Fix typo 'loaded' -> 'load'
+	
+window.addEventListener('loaded', function cookieConsent (){
   if (!getCookie('purecookieDismiss')) {
     document.querySelector('div#page').innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><span>' + purecookieTitle + '</span></div><div class="cookieDesc"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><div class="cookieButton"><button onClick="purecookieDismiss();">' + purecookieButton + '</button></div></div>';
-    pureFadeIn("cookieConsentContainer");
+	pureFadeIn("cookieConsentContainer");
   }
 });
 
 function purecookieDismiss() {
-  setCookie('purecookieDismiss', '1', 7);
+  setCookie('purecookieDismiss','1',7);
   pureFadeOut("cookieConsentContainer");
 }
