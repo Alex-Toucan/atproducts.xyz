f8b1eafea5502daaa3d31a0b44268999a03dedcc
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-09-09 20:56:15 -0500

    Update scroll.js

--- static/scroll.js
+++ static/scroll.js
@@ -2,13 +2,13 @@
     "use strict";
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('fast');
+            $('.back-to-top').fadeIn('slow');
         } else {
-            $('.back-to-top').fadeOut('fast');
+            $('.back-to-top').fadeOut('slow');
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 750, 'easeInOutExpo');
         return false;
     });
 
