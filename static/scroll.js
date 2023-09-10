d2dbb2458164a5206bdd902f137400cbb3ce86ef
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-09-09 20:40:04 -0500

    Update scroll.js

--- static/scroll.js
+++ static/scroll.js
@@ -2,9 +2,9 @@
     "use strict";
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
+            $('.back-to-top').fadeIn('fast');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $('.back-to-top').fadeOut('fast');
         }
     });
     $('.back-to-top').click(function () {
