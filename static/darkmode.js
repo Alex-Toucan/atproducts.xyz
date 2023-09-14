93916275ca925d735713ed257cba8a6c8d1762d0
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-09-14 11:09:55 -0500

    Update darkmode.js

--- static/darkmode.js
+++ static/darkmode.js
@@ -61,7 +61,7 @@
     }
   })
 
-  window.addEventListener('load', () => {
+  window.addEventListener('DOMContentLoaded', () => {
     showActiveTheme(getPreferredTheme())
 
     for (const toggle of document.querySelectorAll('[data-bs-theme-value]')) {
