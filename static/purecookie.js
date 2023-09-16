400a180ee8257c378b8889d3a431b5b47a9cb94f
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-09-15 21:28:16 -0500

    Update purecookie.js

--- static/purecookie.js
+++ static/purecookie.js
@@ -64,11 +64,11 @@ function eraseCookie(name) {
 }
 
 $(window).on("load", function cookieConsent() {
-          if (!getCookie('purecookieDismiss')) {
-    				document.querySelector('div#page').innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><span>' + purecookieIcon + '' + purecookieTitle + '</span></div><div class="cookieDesc"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><div class="cookieButton"><button onClick="purecookieDismiss();">' + purecookieButton + '</button></div></div>';
-						pureFadeIn("cookieConsentContainer");
-					}
-    });
+  if (!getCookie('purecookieDismiss')) {
+    document.querySelector('div#page').innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><span>' + purecookieIcon + '' + purecookieTitle + '</span></div><div class="cookieDesc"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><div class="cookieButton"><button onClick="purecookieDismiss();">' + purecookieButton + '</button></div></div>';
+    pureFadeIn("cookieConsentContainer");
+	}
+});
 
 function purecookieDismiss() {
   setCookie('purecookieDismiss','1',7);
