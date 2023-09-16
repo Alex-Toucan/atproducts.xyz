dab1cf2193baee0ce2060e3946f1b1e7177563ed
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-09-15 21:54:10 -0500

    Update purecookie.js

--- static/purecookie.js
+++ static/purecookie.js
@@ -65,7 +65,7 @@ function eraseCookie(name) {
 
 document.addEventListener('DOMContentLoaded', () => {
       if (!getCookie('purecookieDismiss')) {
-        document.querySelector('div#page').innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><span>' + purecookieTitle + '</span></div><div class="cookieDesc"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><div class="cookieButton"><button onClick="purecookieDismiss();">' + purecookieButton + '</button></div></div>';
+        document.querySelector('div#page').innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><span>' + purecookieIcon + '' + purecookieTitle + '</span></div><div class="cookieDesc"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><div class="cookieButton"><button onClick="purecookieDismiss();">' + purecookieButton + '</button></div></div>';
     	pureFadeIn("cookieConsentContainer");
       }
 });
