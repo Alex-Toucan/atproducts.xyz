5bab8f84572881054faa7f96d961c11ea0dd1d8e
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-09-27 17:35:01 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -29,65 +29,40 @@ $(function () {
     };
 });
 
-$(document).ready(function () {
-  // Initialize the active button and set btn-primary for past years
-  let activeYear = 1;
+$(document).ready(function() {
+    // Initialize the active button and set btn-primary for past years
+    let activeYear = 1;
 
-  updateTimeline(activeYear);
+    updateTimeline(activeYear);
 
-  // Add click event handlers to timeline buttons
-  for (let i = 1; i <= 4; i++) {
-    $(`#history-${i}`).click(function () {
-      if (i == activeYear) return;
-
-      updateTimeline(i);
-    });
-  }
-
-  // Add keyboard event listeners for arrow keys
-  $(document).keydown(function (e) {
-    if (e.which === 37 || e.which === 39) {
-      // Left or Right arrow key
-      e.preventDefault(); // Prevent scrolling on arrow key press
-      const direction = e.which === 37 ? -1 : 1;
-      navigateTimeline(direction);
-    }
-  });
-
-  // Add keyboard event listener for tab key
-  $(document).keydown(function (e) {
-    if (e.which === 9) {
-      // Tab key
-      $(document).on('keyup.tab', function (e) {
-        if (e.which === 9) {
-          e.preventDefault(); // Prevent default tab behavior
-          navigateTimeline(1);
-          $(document).off('keyup.tab'); // Remove the event listener after handling the tab key
-        }
-      });
-    }
-  });
-
-  // Function to update the timeline based on the selected year
-  function updateTimeline(year) {
-    // Remove the "active" class from all buttons
+    // Add click event handlers to timeline buttons
     for (let i = 1; i <= 4; i++) {
-      $(`#history-${i}`).toggleClass("active", i == year).toggleClass("btn-secondary", i > year).toggleClass("btn-primary", i <= year);
+        $(`#history-${i}`).click(function() {
+            if (i == activeYear) return;
+            
+            updateTimeline(i);
+        });
+
+        // Add keydown event listener for Enter key when button is focused
+        $(`#history-${i}`).keydown(function(e) {
+            if (e.key === "Enter") {
+                updateTimeline(i);
+            }
+        });
     }
 
-    // Update the progress bar width
-    const progressBarWidth = (year - 1) * 33.33333333;
-    $("#history-progress").css("width", `${progressBarWidth}%`);
+    // Function to update the timeline based on the selected year
+    function updateTimeline(year) {
+        // Remove the "active" class from all buttons
+        for (let i = 1; i <= 4; i++) {
+            $(`#history-${i}`).toggleClass("active", i == year).toggleClass("btn-secondary", i > year).toggleClass("btn-primary", i <= year);
+        }
 
-    // Update the active year
-    activeYear = year;
-  }
+        // Update the progress bar width
+        const progressBarWidth = (year - 1) * 33.33333333;
+        $("#history-progress").css("width", `${progressBarWidth}%`);
 
-  // Function to navigate the timeline using arrow keys
-  function navigateTimeline(direction) {
-    const newYear = Math.min(Math.max(activeYear + direction, 1), 4);
-    if (newYear !== activeYear) {
-      updateTimeline(newYear);
+        // Update the active year
+        activeYear = year;
     }
-  }
 });
