7e3af3d3de7c26d9774a230922bcb529cd4a690d
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-09-27 18:04:55 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -39,17 +39,8 @@ $(document).ready(function() {
     for (let i = 1; i <= 4; i++) {
         $(`#history-${i}`).click(function() {
             if (i == activeYear) return;
-
+            
             updateTimeline(i);
-        });
-
-        // Add keydown event listener for arrow keys when button is focused
-        $(`#history-${i}`).on('keydown', function(e) {
-            if (e.key === "ArrowLeft" && i > 1) {
-                updateTimeline(i - 1);
-            } else if (e.key === "ArrowRight" && i < 4) {
-                updateTimeline(i + 1);
-            }
         });
     }
 
@@ -60,18 +51,12 @@ $(document).ready(function() {
             $(`#history-${i}`).toggleClass("active", i == year).toggleClass("btn-secondary", i > year).toggleClass("btn-primary", i <= year);
         }
 
+
         // Update the progress bar width
         const progressBarWidth = (year - 1) * 33.33333333;
         $("#history-progress").css("width", `${progressBarWidth}%`);
 
         // Update the active year
         activeYear = year;
-
-        // Hide all tab panes
-        $(".tab-pane").removeClass("active show");
-
-        // Show the active tab pane
-        $(`#history-${year}-pane`).addClass("active show");
     }
 });
-
