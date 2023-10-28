a20f26ce647e36d9be5be9549b1cb122abbc4ffa
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-10-28 16:42:46 -0500

    Update search.js

--- search/search.js
+++ search/search.js
@@ -1,8 +1,39 @@
-$(document).ready(function () {
-    $("#search").on("keyup", function () {
-        let value = $(this).val().toLowerCase();
-        $("#content li").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+// Function to perform the search on the current page
+function performSearch(query) {
+    query = query.toLowerCase();
+    const results = [];
+
+    // Get all the text content on the page
+    const pageText = $("body").text().toLowerCase();
+
+    // Find all occurrences of the query in the page text
+    const matches = pageText.match(new RegExp(query, "g"));
+
+    if (matches) {
+        results.push(...matches);
+    }
+
+    return results;
+}
+
+// Function to update the search results list
+function updateSearchResults(results) {
+    const resultsList = $("#search-results");
+    resultsList.empty();
+
+    if (results.length === 0) {
+        resultsList.append("<li>No results found</li>");
+    } else {
+        results.forEach(result => {
+            resultsList.append("<li>" + result + "</li>");
         });
+    }
+}
+
+$(document).ready(function() {
+    $("#search").on("input", function() {
+        const query = $(this).val();
+        const results = performSearch(query);
+        updateSearchResults(results);
     });
 });
