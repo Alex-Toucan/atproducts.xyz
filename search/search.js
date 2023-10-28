9417f772c62fa227c2478fc2ffaa61843553495f
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-10-28 16:38:26 -0500

    Update search.js

--- search/search.js
+++ search/search.js
@@ -1,32 +1,8 @@
-$(document).ready(function() {
-    // Handle the input event in the search bar
-    $("#searchInput").on("input", function() {
-        const query = $(this).val().toLowerCase();
-        const content = $("#content").text().toLowerCase();
-
-        const results = [];
-
-        // Search for the query within the content
-        if (content.indexOf(query) !== -1) {
-            results.push(query);
-        }
-
-        // Display the search results
-        displayResults(results);
+$(document).ready(function () {
+    $("#search").on("keyup", function () {
+        let value = $(this).val().toLowerCase();
+        $("#content li").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        });
     });
-
-    // Function to display search results
-    function displayResults(results) {
-        const searchResults = $("#searchResults");
-
-        searchResults.empty();
-
-        if (results.length > 0) {
-            results.forEach(function(result) {
-                searchResults.append("<li>" + result + "</li>");
-            });
-        } else {
-            searchResults.html("<li>No results found</li>");
-        }
-    }
 });
