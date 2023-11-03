ce09c06b91b057ddad0d671da1a1dbf0194c9d00
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-03 07:34:31 -0500

    Update search.js

--- search/search.js
+++ search/search.js
@@ -1,35 +1,19 @@
-function getTextNodesIn(elements) {
-    const textNodes = [];
-    elements.forEach(element => {
-        const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
-        while (walker.nextNode()) {
-            textNodes.push(walker.currentNode);
-        }
-    });
-    return textNodes;
-}
-
 function performSearch(query) {
     query = query.toLowerCase();
     const results = [];
-
-    // Get all the text nodes within elements with the class 'searchable'
-    const textNodes = getTextNodesIn(document.querySelectorAll(".searchable"));
-
+    const textNodes = getTextNodesIn(document.querySelector(".searchable"));
     textNodes.forEach(function(node) {
         const text = node.textContent.toLowerCase();
         if (text.includes(query)) {
             results.push(text);
         }
     });
-
     return results;
 }
 
 function updateSearchResults(results) {
     const resultsList = document.getElementById("search-results");
     resultsList.innerHTML = '';
-
     if (results.length === 0) {
         resultsList.innerHTML = "<li>No results found</li>";
     } else {
@@ -41,8 +25,17 @@ function updateSearchResults(results) {
     }
 }
 
+function getTextNodesIn(element) {
+    const textNodes = [];
+    const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+    while (walker.nextNode()) {
+        textNodes.push(walker.currentNode);
+    }
+    return textNodes;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    const searchInput = document.getElementById("search");
+    const searchInput = document.getElementById("search-input");
     searchInput.addEventListener("input", function() {
         const query = searchInput.value;
         const results = performSearch(query);
