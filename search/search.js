c072ec8cfb6b2bb1af922a53a45e3b28e38850fa
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-03 07:31:33 -0500

    Update search.js

--- search/search.js
+++ search/search.js
@@ -1,9 +1,20 @@
+function getTextNodesIn(elements) {
+    const textNodes = [];
+    elements.forEach(element => {
+        const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+        while (walker.nextNode()) {
+            textNodes.push(walker.currentNode);
+        }
+    });
+    return textNodes;
+}
+
 function performSearch(query) {
     query = query.toLowerCase();
     const results = [];
 
-    // Get all the text nodes on the page
-    const textNodes = getTextNodesIn(document.body);
+    // Get all the text nodes within elements with the class 'searchable'
+    const textNodes = getTextNodesIn(document.querySelectorAll(".searchable"));
 
     textNodes.forEach(function(node) {
         const text = node.textContent.toLowerCase();
@@ -16,32 +27,24 @@ function performSearch(query) {
 }
 
 function updateSearchResults(results) {
-    const resultsList = $("#search-results");
-    resultsList.empty();
+    const resultsList = document.getElementById("search-results");
+    resultsList.innerHTML = '';
 
     if (results.length === 0) {
-        resultsList.append("<li>No results found</li>");
+        resultsList.innerHTML = "<li>No results found</li>";
     } else {
         results.forEach(result => {
-            resultsList.append("<li>" + result + "</li>");
+            const li = document.createElement("li");
+            li.textContent = result;
+            resultsList.appendChild(li);
         });
     }
 }
 
-function getTextNodesIn(elements) {
-    const textNodes = [];
-    elements.forEach(element => {
-        const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
-        while (walker.nextNode()) {
-            textNodes.push(walker.currentNode);
-        }
-    });
-    return textNodes;
-}
-
-$(document).ready(function() {
-    $("#search").on("input", function() {
-        const query = $(this).val();
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("search");
+    searchInput.addEventListener("input", function() {
+        const query = searchInput.value;
         const results = performSearch(query);
         updateSearchResults(results);
     });
