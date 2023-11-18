62e6d6b1ecd234b7cd6753cf12fc82058798794c
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-18 10:23:17 -0600

    Update darkmode.js

--- static/darkmode.js
+++ static/darkmode.js
@@ -21,7 +21,7 @@
     if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       document.documentElement.setAttribute('data-bs-theme', 'dark')
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+      document.documentElement.setAttribute('data-bs-theme', 'light')
     }
   }
 
