7348ad7495e5ddbb6dc13627c7fb56ffae60c121
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-19 12:12:06 -0600

    Update modal-id.js

--- static/modal-id.js
+++ static/modal-id.js
@@ -1,5 +1,5 @@
-window.onload = function() {
-  // Get the hash value from the URL
+window.addEventListener('hashchange', function() {
+  // Get the updated hash value from the URL
   var hash = window.location.hash;
 
   // Check if the hash exists
@@ -13,6 +13,19 @@ window.onload = function() {
     // Check if the element exists and if it's a modal
     if (targetElement && targetElement.classList.contains('modal')) {
       // Show the modal
+      var modal = new bootstrap.Modal(targetElement);
+      modal.show();
+    }
+  }
+});
+
+// Initial check on page load
+window.onload = function() {
+  var hash = window.location.hash;
+  if (hash) {
+    var id = hash.substring(1);
+    var targetElement = document.getElementById(id);
+    if (targetElement && targetElement.classList.contains('modal')) {
       var modal = new bootstrap.Modal(targetElement);
       modal.show();
     }
