8cdfe7aa9a64b80dc8440e7d41fe1b4a88a1e468
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-19 22:44:28 -0600

    Update modal-id.js

--- static/modal-id.js
+++ static/modal-id.js
@@ -1,11 +1,11 @@
 window.addEventListener('hashchange', function() {
+  var hash = window.location.hash;
+
   var openModal = document.querySelector('.modal.show');
   if (openModal) {
     var modalInstance = bootstrap.Modal.getInstance(openModal);
     modalInstance.hide();
   }
-
-  var hash = window.location.hash;
 
   if (hash) {
     var id = hash.substring(1);
@@ -15,14 +15,15 @@ window.addEventListener('hashchange', function() {
       var modal = new bootstrap.Modal(targetElement);
       modal.show();
       document.body.classList.add('modal-open');
-
-      // Prevent links within the modal from closing it
-      var modalLinks = targetElement.querySelectorAll('a');
-      modalLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-          event.stopPropagation(); // Prevent the click event from bubbling up
-        });
-      });
+    } else {
+      // If the hash doesn't match any modal IDs, maintain the current modal state
+      // This ensures it doesn't interfere with other modals
+      var modalToKeepOpen = document.querySelector('.modal.show');
+      if (modalToKeepOpen) {
+        document.body.classList.add('modal-open');
+      } else {
+        document.body.classList.remove('modal-open');
+      }
     }
   } else {
     document.body.classList.remove('modal-open');
@@ -38,13 +39,13 @@ window.onload = function() {
       var modal = new bootstrap.Modal(targetElement);
       modal.show();
       document.body.classList.add('modal-open');
-
-      var modalLinks = targetElement.querySelectorAll('a');
-      modalLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-          event.stopPropagation(); // Prevent the click event from bubbling up
-        });
-      });
     }
   }
 };
+
+// Prevent links within modals from closing them
+document.querySelectorAll('.modal a').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent the click event from bubbling up
+  });
+});
