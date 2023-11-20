c0071d65e30c4a0d3f4996122aa61616ba8f0399
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-19 22:39:34 -0600

    Update modal-id.js

--- static/modal-id.js
+++ static/modal-id.js
@@ -1,29 +1,19 @@
 window.addEventListener('hashchange', function() {
-  // Close any open modal if present
   var openModal = document.querySelector('.modal.show');
   if (openModal) {
     var modalInstance = bootstrap.Modal.getInstance(openModal);
     modalInstance.hide();
   }
 
-  // Get the updated hash value from the URL
   var hash = window.location.hash;
 
-  // Check if the hash exists
   if (hash) {
-    // Extract the ID from the hash
-    var id = hash.substring(1); // Remove the '#' symbol
-
-    // Find the element by the extracted ID
+    var id = hash.substring(1);
     var targetElement = document.getElementById(id);
 
-    // Check if the element exists and if it's a modal
     if (targetElement && targetElement.classList.contains('modal')) {
-      // Show the modal
       var modal = new bootstrap.Modal(targetElement);
       modal.show();
-
-      // Add 'modal-open' class to the body tag
       document.body.classList.add('modal-open');
 
       // Prevent links within the modal from closing it
@@ -35,12 +25,10 @@ window.addEventListener('hashchange', function() {
       });
     }
   } else {
-    // Remove 'modal-open' class from the body tag if no hash is present
     document.body.classList.remove('modal-open');
   }
 });
 
-// Initial check on page load
 window.onload = function() {
   var hash = window.location.hash;
   if (hash) {
@@ -49,11 +37,8 @@ window.onload = function() {
     if (targetElement && targetElement.classList.contains('modal')) {
       var modal = new bootstrap.Modal(targetElement);
       modal.show();
-
-      // Add 'modal-open' class to the body tag on initial load
       document.body.classList.add('modal-open');
 
-      // Prevent links within the modal from closing it
       var modalLinks = targetElement.querySelectorAll('a');
       modalLinks.forEach(link => {
         link.addEventListener('click', function(event) {
