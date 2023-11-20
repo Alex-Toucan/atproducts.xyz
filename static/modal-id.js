3f892b59c50132034ca073177926760efc2f1140
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-19 22:06:36 -0600

    Update modal-id.js

--- static/modal-id.js
+++ static/modal-id.js
@@ -1,40 +1,40 @@
-window.addEventListener('hashchange', function(event) {
-  // Close any open modal if present
+// Event listener for close buttons within modals
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('modal-close')) {
+    var openModal = document.querySelector('.modal.show');
+    if (openModal) {
+      var modalInstance = bootstrap.Modal.getInstance(openModal);
+      modalInstance.hide();
+
+      // Remove 'modal-open' class when a modal is closed via close button
+      document.body.classList.remove('modal-open');
+    }
+  }
+});
+
+// Your existing code for handling hash changes
+window.addEventListener('hashchange', function() {
   var openModal = document.querySelector('.modal.show');
   if (openModal) {
     var modalInstance = bootstrap.Modal.getInstance(openModal);
     modalInstance.hide();
   }
 
-  // Get the updated hash value from the URL
   var hash = window.location.hash;
-
-  // Check if the hash exists
   if (hash) {
-    // Extract the ID from the hash
-    var id = hash.substring(1); // Remove the '#' symbol
-
-    // Find the element by the extracted ID
+    var id = hash.substring(1);
     var targetElement = document.getElementById(id);
-
-    // Check if the element exists and if it's a modal
     if (targetElement && targetElement.classList.contains('modal')) {
-      // Show the modal
       var modal = new bootstrap.Modal(targetElement);
       modal.show();
-
-      // Add 'modal-open' class to the body tag
       document.body.classList.add('modal-open');
     }
   } else {
-    // Remove 'modal-open' class from the body tag if no hash is present
     document.body.classList.remove('modal-open');
   }
-
-  event.stopPropagation(); // Stop event propagation
 });
 
-// Initial check on page load
+// Your existing code for initial check on page load
 window.onload = function() {
   var hash = window.location.hash;
   if (hash) {
@@ -43,16 +43,7 @@ window.onload = function() {
     if (targetElement && targetElement.classList.contains('modal')) {
       var modal = new bootstrap.Modal(targetElement);
       modal.show();
-
-      // Add 'modal-open' class to the body tag on initial load
       document.body.classList.add('modal-open');
     }
   }
 };
-
-// Prevent modal closing when clicking inside the modal content
-document.querySelectorAll('.modal').forEach(function(modal) {
-  modal.addEventListener('click', function(event) {
-    event.stopPropagation();
-  });
-});
