019f00c58fc5db79ce83eaa9533f5d74c6621471
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-20 07:51:42 -0600

    Update modal-id.js

--- static/modal-id.js
+++ static/modal-id.js
@@ -1,51 +1,37 @@
 window.onload = function() {
-  // Function to open the modal if the URL contains a modal ID
-  function openModalFromURL() {
-    const hash = window.location.hash.substring(1); // Get the hash from the URL
-    const modal = document.getElementById(hash); // Find the modal by ID
-
-    if (modal) {
-      const $modal = $(`#${hash}`);
-
-      if ($modal.hasClass('modal')) {
-        if (!$modal.hasClass('show')) {
-          $modal.modal('show'); // Show the modal
-        }
-      }
-    } else {
-      const targetElement = document.getElementById(hash); // Find the element by ID
-
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' }); // Scroll to the element smoothly
-      }
+  // Function to check if the hash matches any modal and open it
+  function openModalFromHash() {
+    const hash = window.location.hash;
+    const modal = document.querySelector(hash);
+    if (modal && modal.classList.contains('modal')) {
+      $('.modal').modal('hide'); // Close any open modal
+      $('body').addClass('modal-open');
+      $(hash).modal('show');
     }
   }
 
-  // Function to close any currently open modal
-  function closeModal() {
-    $('.modal').modal('hide'); // Hide any open modal
-  }
+  // Event listener for hash changes
+  window.addEventListener('hashchange', openModalFromHash);
 
-  // Event listener for hash changes to open the modal
-  window.addEventListener('hashchange', function() {
-    closeModal(); // Close any open modal before opening a new one
-    openModalFromURL(); // Open the modal from the URL hash
+  // Open modal on page load if hash present
+  openModalFromHash();
+
+  // Handle clicks on links within modals
+  $('.modal').on('click', 'a[href^="#"]', function(e) {
+    const hash = $(this).attr('href');
+    const targetModal = $(hash);
+    if (targetModal.length && targetModal.hasClass('modal')) {
+      e.preventDefault();
+      window.location.hash = hash;
+    }
   });
 
-  openModalFromURL(); // Call the function when the page loads
-
-  // Function to prevent closing modal when clicking on ID links within the modal
-  function preventModalCloseOnInternalLinks() {
-    $('.modal').on('click', 'a[href^="#"]', function(e) {
-      const hash = $(this).attr('href').substring(1); // Get the href hash value
-      const target = document.getElementById(hash); // Find the element by ID
-
-      if (!$(target).closest('.modal').length) {
-        e.preventDefault(); // Prevent the default link behavior
-        window.location.hash = hash; // Change the URL hash without closing the modal
-      }
-    });
-  }
-
-  preventModalCloseOnInternalLinks(); // Call the function to prevent modal close on internal links
+  // Handle case when ID in URL doesn't match a modal
+  $(document).on('click', 'a[href^="#"]', function() {
+    const hash = $(this).attr('href');
+    const modal = $(hash);
+    if (!modal.length || !modal.hasClass('modal')) {
+      window.location.hash = ''; // Clear hash to close modal
+    }
+  });
 };
