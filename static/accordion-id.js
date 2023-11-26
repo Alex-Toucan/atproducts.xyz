1a4ea0210a46139648e40a1e47da86455603eb9f
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-26 00:40:09 -0600

    Update accordion-id.js

--- static/accordion-id.js
+++ static/accordion-id.js
@@ -18,18 +18,12 @@ $(document).ready(function() {
         $accordionSection.addClass('show'); // Open the accordion section corresponding to the hash
         var $heading = $accordionSection.closest('.accordion-item').find('.accordion-button');
 
-        $heading.removeClass('collapsed').attr('aria-expanded', 'true'); // Update attributes for the specific accordion button
+        if ($heading.length) {
+          $heading.removeClass('collapsed').attr('aria-expanded', 'true'); // Update attributes for the specific accordion button
 
-        // Scroll to the opened accordion section with an offset above the button
-        if (!$('body').hasClass('modal-open')) {
-          /* nothing here */
-        }
-        else {
-          var $heading = $(this).find('.accordion-button'); // Find the accordion button within the modal
-          if ($heading.length) {
-            var offset = $heading.offset().top - 100; // Adjust the offset as needed
-            $('html, body').animate({ scrollTop: offset }, 'slow');
-          }
+          // Scroll to the opened accordion section with an offset above the button
+          var offset = $heading.offset().top - 100; // Adjust the offset as needed
+          $('html, body').animate({ scrollTop: offset }, 'slow');
         }
       }
     }
@@ -43,4 +37,16 @@ $(document).ready(function() {
   // Initial execution to open accordion based on hash
   openAccordionFromHash();
 
+  // Prevent scrolling when a modal is open
+  $(document).on('show.bs.modal', '.modal', function() {
+    if ($('body').hasClass('modal-open')) {
+      // This is the section that prevents scrolling when a modal is open
+      // Here, you can perform actions related to preventing scrolling
+      var $heading = $(this).find('.accordion-button'); // Find the accordion button within the modal
+      if ($heading.length) {
+        var offset = $heading.offset().top - 100; // Adjust the offset as needed
+        $('html, body').animate({ scrollTop: offset }, 'slow');
+      }
+    }
+  });
 });
