dd9ab7f6aae2b55244915bb927227226b68a19f3
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-26 10:58:03 -0600

    Update accordion-id.js

--- static/accordion-id.js
+++ static/accordion-id.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  let isUserScrolling = false;
-
   // Function to check if the hash matches any accordion and open it
   function openAccordionFromHash() {
     const hash = window.location.hash;
@@ -10,6 +8,12 @@ $(document).ready(function() {
         if (!accordion.hasClass('show')) {
           $('.collapse.show').collapse('hide'); // Hide any open accordion
           accordion.collapse('show');
+          const $heading = $('[href="' + hash + '"]');
+          if ($heading.length) {
+            $heading.removeClass('collapsed').attr('aria-expanded', 'true');
+            const offset = $heading.offset().top - 100;
+            $('html, body').animate({ scrollTop: offset }, 'slow');
+          }
         }
       }
     }
@@ -23,42 +27,37 @@ $(document).ready(function() {
 
   // Handle clicks on accordion headers and URL hash changes
   $(document).on('click', '.accordion-button', function() {
-    const accordion = $(this).closest('.accordion-header').next('.collapse');
-    if (accordion.length) {
-      const hash = '#' + accordion.attr('id');
-      if (window.location.hash !== hash) {
-        window.location.hash = hash;
-      } else {
-        window.location.hash = ''; // Clear hash to close accordion
-      }
+    const $heading = $(this);
+    if ($heading.length) {
+      $heading.removeClass('collapsed').attr('aria-expanded', 'true');
+      const offset = $heading.offset().top - 100;
+      $('html, body').animate({ scrollTop: offset }, 'slow');
     }
   });
 
   // Scroll to accordion item when page scrolls above 100px
   $(window).on('scroll', function() {
-    if (!isUserScrolling) {
-      const accordionItem = $('.accordion-item');
-      if (accordionItem.length) {
-        const accordionOffset = accordionItem.offset().top - 50; // Adjust the offset as needed
-        if ($(window).scrollTop() > 100 && $(window).scrollTop() < accordionOffset) {
-          $('html, body').animate({
-            scrollTop: accordionOffset
-          }, 500);
-        }
+    const accordionItem = $('.accordion-item');
+    if (accordionItem.length) {
+      const accordionOffset = accordionItem.offset().top - 100; // Adjust the offset as needed
+      if ($(window).scrollTop() > 100 && $(window).scrollTop() < accordionOffset) {
+        $('html, body').scrollTop(accordionOffset);
       }
     }
   });
 
-  // Detect user-initiated scroll
-  $(window).on('wheel', function() {
-    isUserScrolling = true;
-    setTimeout(function() {
-      isUserScrolling = false;
-    }, 100);
-  });
-
   // Reset padding-right for .navbar when accordion is fully shown
   $(document).on('shown.bs.collapse', '.collapse', function() {
-    // Perform additional actions after the accordion is fully shown if needed
+    // ... (existing actions after accordion shown remains unchanged)
+  });
+
+  // Detect user-initiated scroll and handle interruption
+  let lastScrollTop = 0;
+  $(window).on('scroll', function() {
+    const st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+      $('html, body').stop();
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
   });
 });
