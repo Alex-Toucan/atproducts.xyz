1ec4a220b541c2bd08b19f478815a5b2f47dd9de
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-25 23:20:04 -0600

    Update accordion-id.js

--- static/accordion-id.js
+++ static/accordion-id.js
@@ -1,17 +1,19 @@
-// Function to open the accordion based on hash change
-function openAccordionFromHash() {
-  var hash = window.location.hash;
-  if (hash !== '') {
-    $('.accordion-button').addClass('collapsed');
-    $('.accordion-collapse').removeClass('show');
-    $(hash).collapse('show');
+$(document).ready(function() {
+  // Function to open the accordion based on hash change
+  function openAccordionFromHash() {
+    var hash = window.location.hash;
+    if (hash !== '') {
+      $('.accordion-button').addClass('collapsed');
+      $('.accordion-collapse').removeClass('show');
+      $(hash).collapse('show');
+    }
   }
-}
 
-// Function to handle hash change event
-$(window).on('hashchange', function() {
+  // Function to handle hash change event
+  $(window).on('hashchange', function() {
+    openAccordionFromHash();
+  });
+
+  // Initial execution to open accordion based on hash
   openAccordionFromHash();
 });
-
-// Initial execution to open accordion based on hash
-openAccordionFromHash();
