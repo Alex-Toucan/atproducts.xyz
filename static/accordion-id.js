7328b472433aadddbdeeeab338e08847677c7ea6
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-25 23:12:41 -0600

    Update accordion-id.js

--- static/accordion-id.js
+++ static/accordion-id.js
@@ -1,13 +1,38 @@
 $(document).ready(function() {
+  // Initialize all elements as collapsible
+  $('.accordion').accordion({
+    collapsible: true,
+    active: false,
+    heightStyle: 'content', // Adjusts the height based on content
+    activate: function(event, ui) {
+      // When an accordion element is clicked, update the hash
+      const activeId = ui.newHeader.attr('id');
+      if (activeId) {
+        window.location.hash = `#${activeId}`;
+      }
+    }
+  });
+
   function openElementFromHash() {
     const hash = window.location.hash;
     if (hash !== '') {
       const elementId = hash.substr(1);
-      $(`#${elementId}`).show(); // Show the element directly
+      $(`#${elementId}`).accordion('option', 'active', 0); // Open the element based on hash
     }
   }
 
   openElementFromHash();
 
   $(window).on('hashchange', openElementFromHash);
+
+  // Trigger element based on user input
+  $('#openElementBtn').on('click', function() {
+    const userInput = $('#inputId').val();
+    if (userInput !== '') {
+      // Close all elements
+      $('.accordion').accordion('option', 'active', false);
+      // Open the selected element
+      $(`#${userInput}`).accordion('option', 'active', 0);
+    }
+  });
 });
