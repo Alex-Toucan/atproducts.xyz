e2f9d596aceebeff062ab51cd95895367ff5ea10
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-26 11:50:01 -0600

    Update accordion-id.js

--- static/accordion-id.js
+++ static/accordion-id.js
@@ -6,23 +6,19 @@ $(document).ready(function() {
       const targetId = hash.substring(1); // Remove '#' from the hash
 
       const accordion = $('#' + targetId); // Use the ID directly
-      console.log('Accordion:', accordion);
 
       if (accordion.length && accordion.hasClass('collapse')) {
         if (!accordion.hasClass('show')) {
           $('.collapse.show').collapse('hide');
           accordion.collapse('show');
           const element = document.getElementById(targetId);
-          console.log('Element:', element);
 
           // Scroll to the element with padding and animation
           if (element) {
             const paddingTop = 150; // 150px
-            const targetOffset = $(element).offset().top;
-            const scrollPosition = targetOffset - $(window).scrollTop();
-            const adjustedScroll = scrollPosition - paddingTop;
+            const targetOffset = $(element).offset().top - paddingTop;
             $('html, body').animate({
-              scrollTop: $(window).scrollTop() + adjustedScroll
+              scrollTop: targetOffset
             }, 800);
           }
         }
