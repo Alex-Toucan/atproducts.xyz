f10c33bc10effa97808a00a276a4c24de8629c22
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-11-26 00:37:09 -0600

    Update accordion-id.js

--- static/accordion-id.js
+++ static/accordion-id.js
@@ -5,53 +5,53 @@ $(document).ready(function() {
     if (hash !== '' && $('.modal.show').length === 0) {
       var $accordionSection = $(hash);
       if ($accordionSection.length && $accordionSection.hasClass('accordion-collapse') && !$accordionSection.hasClass('show')) {
+        // Check if the accordion is within a modal
         var $modal = $accordionSection.closest('.modal');
         if ($modal.length && !$modal.hasClass('show')) {
+          // If the accordion is within a closed modal, open the modal first
           $modal.modal('show');
         }
 
-        $('.accordion-collapse').removeClass('show');
-        $('.accordion-button').addClass('collapsed').attr('aria-expanded', 'false');
+        $('.accordion-collapse').removeClass('show'); // Close all accordion sections
+        $('.accordion-button').addClass('collapsed').attr('aria-expanded', 'false'); // Reset all accordion buttons
 
-        $accordionSection.addClass('show');
+        $accordionSection.addClass('show'); // Open the accordion section corresponding to the hash
         var $heading = $accordionSection.closest('.accordion-item').find('.accordion-button');
 
-        if ($heading.length) {
-          $heading.removeClass('collapsed').attr('aria-expanded', 'true');
+        $heading.removeClass('collapsed').attr('aria-expanded', 'true'); // Update attributes for the specific accordion button
 
-          var offset = $heading.offset().top - 100;
-          $('html, body').animate({ scrollTop: offset }, 'slow');
+        // Scroll to the opened accordion section with an offset above the button
+        if (!$('body').hasClass('modal-open')) {
+          /* nothing here */
         }
-        changeAccordionIDs($modal); // Change accordion IDs within the modal after it's shown
+        else {
+          var $heading = $(this).find('.accordion-button'); // Find the accordion button within the modal
+          if ($heading.length) {
+            var offset = $heading.offset().top - 100; // Adjust the offset as needed
+            $('html, body').animate({ scrollTop: offset }, 'slow');
+          }
+        }
       }
     }
   }
-  
-  function changeAccordionIDs($modal) {
-    if ($modal) {
-      $modal.find('.accordion-collapse').each(function(index) {
-        var newID = 'accordionItem_' + index;
-        $(this).attr('id', newID);
-        // Update the button aria-controls attribute with the new ID
-        var $button = $(this).prev('.accordion-button');
-        $button.attr('aria-controls', newID);
-      });
-    }
-  }
 
+  // Function to handle hash change event
   $(window).on('hashchange', function() {
     openAccordionFromHash();
   });
 
+  // Initial execution to open accordion based on hash
+  openAccordionFromHash();
+
   // Prevent scrolling when a modal is open
   $(document).on('show.bs.modal', '.modal', function() {
     if (!$('body').hasClass('modal-open')) {
-      // Scroll logic or adjustments if needed
-    } else {
-      var $heading = $(this).find('.accordion-button');
+      /* nothing here */
+    }
+    else {
+      var $heading = $(this).find('.accordion-button'); // Find the accordion button within the modal
       if ($heading.length) {
-        var offset = $heading.offset().top - 100;
-        $('html, body').animate({ scrollTop: offset }, 'slow');
+        var offset = $heading.offset().top - 100; // Adjust the offset as needed
       }
     }
   });
