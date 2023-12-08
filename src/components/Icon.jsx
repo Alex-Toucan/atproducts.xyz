c4679fa90f9a05b082d9b8196b41764180869ebf
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-08 12:12:56 -0600

    Update and rename Icon.astro to Icon.jsx

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,6 +1,6 @@
 <:astro>
   export default function Icon({ href, icon, desc }) {
-    {if href && icon && desc}
+    {#if href && icon && desc}
       return (
         <a href={href} target='_blank' rel='noopener noreferrer' class='link-hero fs-7'>
           <i class={`bi bi-${icon}`}></i>
