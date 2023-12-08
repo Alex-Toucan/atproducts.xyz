95308ca40ebc3a07476558e2681b52614b208696
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-08 12:17:37 -0600

    Update Icon.jsx

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,11 +1,11 @@
 export default function Icon({ href, icon, desc }) {
-    {#if href && icon && desc}
+    {href && icon && desc (
       return (
         <a href={href} target='_blank' rel='noopener noreferrer' class='link-hero fs-7'>
           <i class={`bi bi-${icon}`}></i>
           <span>{desc}</span>
         </a>
-      );
-    {/if}
+    ) : (
     return null;
+     )};
 }
