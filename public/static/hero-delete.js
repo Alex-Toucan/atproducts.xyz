3269014598aabaf6c850ea8eaa7cff0352315a19
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-07 21:12:09 -0600

    Update hero-delete.js

--- public/static/hero-delete.js
+++ public/static/hero-delete.js
@@ -1,59 +1,24 @@
-const dropdownItems = [
-  { navigationdesc: iddesc1, navigationlink: idhref1 },
-  { navigationdesc: iddesc2, navigationlink: idhref2 },
-  { navigationdesc: iddesc3, navigationlink: idhref3 },
-  { navigationdesc: iddesc4, navigationlink: idhref4 },
-  { navigationdesc: iddesc5, navigationlink: idhref5 },
-  { navigationdesc: iddesc6, navigationlink: idhref6 },
-  { navigationdesc: iddesc7, navigationlink: idhref7 },
-  { navigationdesc: iddesc8, navigationlink: idhref8 },
-  { navigationdesc: iddesc9, navigationlink: idhref9 },
-  { navigationdesc: iddesc10, navigationlink: idhref10 },
-];
+// Function to remove elements based on condition
+function removeElements() {
+  // Remove empty dropdown items
+  const dropdownItems = document.querySelectorAll('.dropdown-item');
+  dropdownItems.forEach((item) => {
+    if (!item.textContent.trim()) {
+      item.remove();
+    }
+  });
 
-const icons = [
-  { icon: icon1, desc: desc1, href: href1 },
-  { icon: icon2, desc: desc2, href: href2 },
-  { icon: icon3, desc: desc3, href: href3 },
-  { icon: icon4, desc: desc4, href: href4 },
-  { icon: icon5, desc: desc5, href: href5 }
-];
+  // Remove undefined icon elements
+  const iconLinks = document.querySelectorAll('.link-hero.fs-7');
+  iconLinks.forEach((link) => {
+    const iconText = link.querySelector('i').className;
+    if (iconText.includes('undefined')) {
+      link.remove();
+    }
+  });
+}
 
-// Fetching the lengths of the arrays
-const numItems = Math.min(dropdownItems.length, 10); // Set the number of items you want in the dropdown, up to a maximum of 10
-const numLinks = Math.min(icons.length, 10); // Set the number of links you want in the icon bar, up to a maximum of 10
-
-// Filtered icons based on the number of links required
-const filteredIcons = icons.slice(0, numLinks).filter(({ icon, desc, href }) => icon && desc && href);
-
-// Filtered dropdown items based on the number of items required
-const filteredDropdownItems = dropdownItems.slice(0, numItems).filter(({ navigationdesc, navigationlink }) => navigationdesc && navigationlink);
-
-// Rendering the filtered icons
-filteredIcons.forEach(({ icon, desc, href }, index) => {
-  const linkElement = document.createElement('a');
-  linkElement.href = href;
-  linkElement.target = '_blank';
-  linkElement.rel = 'noopener noreferrer';
-  linkElement.classList.add('link-hero', 'fs-7');
-  linkElement.setAttribute('data-bs-toggle', 'tooltip');
-  linkElement.setAttribute('data-bs-placement', 'bottom');
-  linkElement.setAttribute('data-bs-title', desc);
-
-  const iconElement = document.createElement('i');
-  iconElement.classList.add(`bi`, `bi-${icon}`);
-
-  linkElement.appendChild(iconElement);
-  document.getElementById('icons').appendChild(linkElement);
-});
-
-// Rendering the filtered dropdown items
-filteredDropdownItems.forEach(({ navigationdesc, navigationlink }, index) => {
-  const listItem = document.createElement('li');
-  const linkElement = document.createElement('a');
-  linkElement.classList.add('dropdown-item');
-  linkElement.href = navigationlink;
-  linkElement.textContent = navigationdesc;
-  listItem.appendChild(linkElement);
-  document.querySelector('.dropdown-menu').appendChild(listItem);
+// Call the function when the document is loaded
+document.addEventListener('DOMContentLoaded', () => {
+  removeElements();
 });
