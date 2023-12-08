743ff7e3c5dbc654ebefcac793783c671f41aaef
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-08 12:42:44 -0600

    Update Icon.jsx

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,1 +1,13 @@
-// Temp disabled
+render() {
+  const icon = ({ href, icon, desc });
+  return (
+      {href && icon && desc ? (
+       <a href={href} target="_blank" rel="noopener noreferrer" class="link-hero fs-7" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title={desc}>
+          <i class={`bi bi-${icon}`}></i>
+        </a>
+      ) : (
+        return null;
+      )}
+    </div>
+  );
+}
