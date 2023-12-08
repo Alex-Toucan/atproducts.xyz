45c8401f589cc64110ff7b62f0be2d86500e8a48
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-08 12:56:06 -0600

    Update Icon.jsx

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,14 +1,12 @@
 function iconrender() {
   const icon = ({ href, icon, desc });
   return (
-      {href, icon, desc} ? (
+      if (href) {
        <a href={href} target="_blank" rel="noopener noreferrer" class="link-hero fs-7" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title={desc}>
           <i class={`bi bi-${icon}`}></i>
         </a>
-      ) : (
+      }  
         return null;
-      )
-    </div>
   );
 }
 
