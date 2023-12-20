257dd5136436538674f8b8047ee21b65bebca1f1
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-19 19:08:41 -0600

    a

--- public/static/scrollspy-id.js
+++ public/static/scrollspy-id.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
-    function openScrollspyFromHash() {
+    function scrollToSectionFromHash() {
       const hash = decodeURIComponent(window.location.hash); // Decode the hash
   
       if (hash && hash.startsWith('#')) {
         const targetId = hash.substring(1); // Remove '#' from the hash
+        const targetSection = $('#' + targetId); // Find the section based on ID
   
-        const scrollspy = $('#' + targetId); // Use the ID directly
-        const modal = scrollspy.closest('.modal'); // Find the closest parent modal
+        if (targetSection.length) {
+          const modal = targetSection.closest('.modal'); // Find the closest parent modal
   
-        if (modal && modal.length) {
-          modal.modal('show'); // Open the modal if it exists
+          if (modal && modal.length) {
+            modal.modal('show'); // Open the modal if it exists
+  
+            // Calculate the scroll position to the section within the modal
+            const paddingTop = 50; // Adjust this value if needed
+            const sectionTop = targetSection.offset().top - modal.find('.modal-content').offset().top - paddingTop;
+  
+            // Smooth scroll to the section within the modal
+            modal.find('.modal-body').animate({
+              scrollTop: sectionTop
+            }, 800);
+          }
         }
       }
     }
   
-    $(window).on('hashchange', openScrollspyFromHash);
-    openScrollspyFromHash();
+    // Call scrollToSectionFromHash on hash change and page load
+    $(window).on('hashchange', scrollToSectionFromHash);
+    scrollToSectionFromHash();
   });
   
