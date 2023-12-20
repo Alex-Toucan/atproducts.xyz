f3b18baed0809663ffd45a1cd8870855b7e88733
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-19 19:36:26 -0600

    b

--- public/static/scrollspy-id.js
+++ public/static/scrollspy-id.js
@@ -6,19 +6,21 @@ $(document).ready(function() {
         const targetId = hash.substring(1); // Remove '#' from the hash
         const targetSection = $('#' + targetId); // Find the section based on ID
   
-        const modalWithSection = targetSection.closest('.modal'); // Find the closest parent modal containing the section
+        if (targetSection.length) {
+          const modal = targetSection.closest('.modal'); // Find the closest parent modal
   
-        if (modalWithSection && modalWithSection.length) {
-          modalWithSection.modal('show'); // Open the modal if the section's modal exists
+          if (modal && modal.length) {
+            modal.modal('show'); // Open the modal if it exists
   
-          // Calculate the scroll position to the section within the modal
-          const paddingTop = 50; // Adjust this value if needed
-          const sectionTop = targetSection.offset().top - modalWithSection.find('.modal-content').offset().top - paddingTop;
+            // Calculate the scroll position to the section within the modal
+            const paddingTop = 50; // Adjust this value if needed
+            const sectionTop = targetSection.offset().top - modal.find('.modal-content').offset().top - paddingTop;
   
-          // Smooth scroll to the section within the modal
-          modalWithSection.find('.modal-body').animate({
-            scrollTop: sectionTop
-          }, 800);
+            // Smooth scroll to the section within the modal
+            modal.find('.modal-body').animate({
+              scrollTop: sectionTop
+            }, 800);
+          }
         }
       }
     }
