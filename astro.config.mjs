9607d28e2cc0b86ae1c03f6ba6e177ba1193caca
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-20 21:21:05 -0600

    add more crap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), compress()]
 });
