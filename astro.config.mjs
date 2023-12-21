00295463c99e5a8f96b6ad90b74a2c09092a2d5a
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2023-12-20 21:51:13 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import defineConfig from 'astro/config';
-
-import react from "@astrojs/react";
-
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), compress()]
+})
