4ecf721825fb904c82e0eb4a97b785822c39b81b
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2024-02-15 08:43:39 -0600

    Update accordion-id.js

--- public/static/accordion-id.js
+++ public/static/accordion-id.js
@@ -4,39 +4,30 @@ $(document).ready(function() {
 
     if (hash && hash.startsWith('#')) {
       const targetId = hash.substring(1);
-      const accordion = $('#' + targetId);
-      const modal = accordion.closest('.modal');
+      const childAccordion = $('#' + targetId);
 
-      if (modal && modal.length) {
-        modal.modal('show');
-      }
-
-      if (accordion.length && accordion.hasClass('collapse')) {
-        if (!accordion.hasClass('show')) {
+      if (childAccordion.length && childAccordion.hasClass('collapse')) {
+        if (!childAccordion.hasClass('show')) {
           $('.collapse.show').collapse('hide');
-          accordion.collapse('show');
-          const element = document.getElementById(targetId);
+          childAccordion.collapse('show');
 
+          // Check if the accordion has a parent accordion
+          const parentAccordion = childAccordion.parents('.accordion');
+          if (parentAccordion && parentAccordion.length) {
+            parentAccordion.find('.collapse.show').collapse('hide');
+            parentAccordion.find(childAccordion).collapse('show');
+          }
+
+          const element = document.getElementById(targetId);
           if (element) {
             const paddingTop = 150;
-            const sectionTop = accordion.offset().top - paddingTop;
+            const sectionTop = childAccordion.offset().top - paddingTop;
             $('html, body').animate({
               scrollTop: sectionTop
             }, 800);
           }
         }
       }
-
-      // Check if the accordion is a child and open both child and parent accordions
-      accordion.on('show.bs.collapse', function() {
-        const parentAccordion = accordion.parents('.accordion').first();
-        if (parentAccordion.length) {
-          const childAccordion = parentAccordion.find(`[data-bs-target="#${targetId}"]`);
-          if (childAccordion.length) {
-            childAccordion.collapse('show');
-          }
-        }
-      });
     }
   }
 
