8ad3eb493b26b5bccc1fdf8505e5f2cb36d39154
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2024-02-19 18:26:38 -0600

    Update accordion-id.js

--- public/static/accordion-id.js
+++ public/static/accordion-id.js
@@ -13,14 +13,6 @@ $(document).ready(function() {
         modal.modal('show');
       }
 
-      // Unbind the event handler after it's triggered
-      $('.accordion').one('show.bs.collapse', function(event) {
-        const accordionItem = accordion.closest('.accordion-item');
-        if (accordionItem.length) {
-          accordionItem.parents('.accordion-item').find('.collapse').collapse('show');
-        }
-      });
-
       if (accordion.length && accordion.hasClass('collapse')) {
         if (!accordion.hasClass('show')) {
           $('.collapse.show').collapse('hide');
@@ -36,6 +28,14 @@ $(document).ready(function() {
           }
         }
       }
+      
+      // Unbind the event handler after it's triggered
+      $('.accordion').one('show.bs.collapse', function(event) {
+        const accordionItem = accordion.closest('.accordion-item');
+        if (accordionItem.length) {
+          accordionItem.parents('.accordion-item').find('.collapse').collapse('show');
+        }
+      });
     }
   }
 
