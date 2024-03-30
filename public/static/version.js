0b1d7dd80064802658a009b7bb4d810aa028ce67
Alex Toucan <alextoucan.at@gmail.com>
2024-03-29 23:02:31 -0500

    v2.0.1

--- public/static/version.js
+++ public/static/version.js
@@ -1,1 +1,1 @@
-document.write("DoBT v2.0");
+document.write("DoBT v2.0.1");
--- static/404.js
+++
@@ -1,9 +0,0 @@
-function Previous() {
-  window.history.back()
-}
-
-var fourofour = document.getElementById('404btn');
-
-if(fourofour) {
-  fourofour.addEventListener("click", Previous);
-}
