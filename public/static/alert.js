943c6c08db60c81f4a8f802033c89765dcd63f34
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2024-05-07 12:50:11 -0500

    Making alerts dismissible for longer periods of time. (#200)
    * Update index.astro
    
    * Update Scripts.astro
    
    * Create alert.js
    
    * Update alert.js
    
    * Update Head.astro
    
    * Update alert.js
    
    * Update alert.js
    
    * Update alert.js
    
    * Update alert.js

---
+++ public/static/alert.js
@@ -0,0 +1,43 @@
+// --- Config --- //
+var pureAlertTitle = "NEW BETA TESTING SITE:"; // Title
+var pureAlertIcon = '<i class="bi bi-plus-lg h-100"></i>' // Cookie icon
+var pureAlertDesc = "We have updated the Department of Beta Testing site to be more modernized with Bootstrap 5 and Astro!"; // Description
+var pureAlertLink = 'https://beta-testing.atproducts.xyz'; // Cookiepolicy link
+var pureAlertLinkDesc = 'View now'; // Cookiepolicy link
+var pureAlertButton = "Understood"; // Button text
+// ---        --- //
+
+function setAlertCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getAlertCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseAlertCookie(name) {
+    document.cookie = name+'=; Max-Age=-99999999;';
+}
+
+/* Comment this section if there is no alert loaded */
+document.addEventListener('DOMContentLoaded', () => {
+  if (!getAlertCookie('pureAlertDismiss')) {
+    document.querySelector('header').innerHTML += 
+'<div class="alert alert-dark alert-dismissible fade show mb-0 d-flex gap-2" role="alert">' + pureAlertIcon + '<div><strong>' + pureAlertTitle + '</strong> ' + pureAlertDesc + ' <a class="icon-link icon-link-hover" href="' + pureAlertLink + '">' + pureAlertLinkDesc + '<i class="bi bi-arrow-right h-100"></i></a></div><button onclick="pureAlertDismiss();" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
+  }
+});
+
+function pureAlertDismiss() {
+  setAlertCookie('pureAlertDismiss','1',7);
+}
