e3a989a92d10d63ca2c7ee1db47290451b298b56
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2024-05-07 21:20:58 -0500

    Update alert.js

--- public/static/alert.js
+++ public/static/alert.js
@@ -33,8 +33,11 @@ function eraseAlertCookie(name) {
 /* Comment this section if there is no alert loaded */
 document.addEventListener('DOMContentLoaded', () => {
   if (!getAlertCookie('pureAlertDismiss')) {
-    document.querySelector('header').innerHTML += 
-'<div class="alert alert-dark alert-dismissible fade show mb-0 d-flex gap-2" role="alert">' + pureAlertIcon + '<div><strong>' + pureAlertTitle + '</strong> ' + pureAlertDesc + ' <a class="icon-link icon-link-hover" href="' + pureAlertLink + '">' + pureAlertLinkDesc + '<i class="bi bi-arrow-right h-100"></i></a></div><button onclick="pureAlertDismiss();" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
+    var header = document.querySelector('header');
+    var alertContainer = document.createElement('div');
+    alertContainer.className = "alert-container";
+    alertContainer.innerHTML = '<div class="alert alert-dark alert-dismissible fade show mb-0 d-flex gap-2" role="alert">' + pureAlertIcon + '<div><strong>' + pureAlertTitle + '</strong> ' + pureAlertDesc + ' <a class="icon-link icon-link-hover" href="' + pureAlertLink + '">' + pureAlertLinkDesc + '<i class="bi bi-arrow-right h-100"></i></a></div><button onclick="pureAlertDismiss();" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
+    header.parentNode.insertBefore(alertContainer, header.nextSibling);
   }
 });
 
