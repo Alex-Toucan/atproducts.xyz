2dc4de006978bbe2fdbfda43a2d2906677927ad0
Alex Toucan <alextoucan.at@gmail.com>
2024-06-20 03:25:27 -0500

    Disabling the popovers on the next click

--- public/static/enabler.js
+++ public/static/enabler.js
@@ -3,3 +3,7 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const popover = new bootstrap.Popover('.popover-dismiss', {
+      trigger: 'focus'
+})
