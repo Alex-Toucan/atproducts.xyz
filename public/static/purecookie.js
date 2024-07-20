a2381d29b35be9b236a95b8d5ed8642569890847
Alex Toucan <alextoucan.at@gmail.com>
2024-07-20 15:48:53 -0500

    Update everything to 2.19 standards

--- public/static/purecookie.js
+++ public/static/purecookie.js
@@ -2,75 +2,55 @@
 var purecookieTitle = "Cookies."; // Title
 var purecookieIcon = '<i class="bi bi-cookie me-2"></i>' // Cookie icon
 var purecookieDesc = "By using this website, you automatically accept that we use cookies."; // Description
-var purecookieLink = '<a href="/privacy#cookies" target="_blank" rel="noopener noreferrer">Why?</a>'; // Cookiepolicy link
+var purecookieLink = '<a href="/privacy#cookies" target="_blank" rel="noopener noreferrer">Why?</a>'; // Cookie policy link
 var purecookieButton = "Understood"; // Button text
 // ---        --- //
 
-
-function pureFadeIn(elem, display){
-  var el = document.getElementById(elem);
-  el.style.opacity = 0;
-  el.style.display = display || "block";
-
-  (function fade() {
-    var val = parseFloat(el.style.opacity);
-    if (!((val += .02) > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
-    }
-  })();
-};
-	
-function pureFadeOut(elem){
-  var el = document.getElementById(elem);
-  el.style.opacity = 1;
-
-  (function fade() {
-    if ((el.style.opacity -= .02) < 0) {
-      el.style.display = "none";
-    } else {
-      requestAnimationFrame(fade);
-    }
-    setTimeout(() => {
-      var theelement = document.querySelector('#cookieConsentContainer');
-        if (theelement) {
-          theelement.remove();
-        };
-    }, 1350);
-  })();
-};
-
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+function pureFadeIn(elem, display) {
+  $("#" + elem).css({opacity: 0, display: display || "block"}).animate({opacity: 1}, 500);
 }
+
+function pureFadeOut(elem) {
+  $("#" + elem).animate({opacity: 0}, 500, function(){
+    $(this).css("display", "none").delay(1350).queue(function(){
+      $(this).remove().dequeue();
+    });
+  });
+}
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000*4.28571428571));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-function eraseCookie(name) {
-    document.cookie = name+'=; Max-Age=-99999999;';
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function eraseCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+$(document).ready(function() {
   if (!getCookie('purecookieDismiss')) {
-    document.querySelector('div#page').innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><span>' + purecookieIcon + '' + purecookieTitle + '</span></div><div class="cookieDesc"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><div class="cookieButton"><button onClick="purecookieDismiss();">' + purecookieButton + '</button></div></div>';
+    $('div#page').append('<div class="cookieConsentContainer bg-secondary-subtle rounded-md-3" id="cookieConsentContainer"><div class="h4">' + purecookieIcon + '' + purecookieTitle + '</div><div class="my-3"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><button class="btn btn-dl" onclick="purecookieDismiss();">Understood</button></div>');
     pureFadeIn("cookieConsentContainer");
   }
 });
 
 function purecookieDismiss() {
-  setCookie('purecookieDismiss','1',7);
+  setCookie('purecookieDismiss', '1', 7);
   pureFadeOut("cookieConsentContainer");
 }
