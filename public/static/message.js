6c23bcffad946388abab5a272d80060c5bddf4c4
Alex Toucan <alextoucan.at@gmail.com>
2024-07-24 13:19:57 -0500

    Add message.js

---
+++ public/static/message.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  console.log('%cHello there fellow traveler!%c\nFeel free to explore and experiment with the console. Rest assured, nothing on this site can compromise your security, and your cookies do not contain personally identifiable information.',
+              'font-weight: bold; font-size: 24px; color: green;', // Style for header
+              'font-size: 16px; color: white;'                     // Style for body text
+             );
+});
