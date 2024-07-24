9fe49db0046da197ec860c6cbb7fe3b833f09a15
Alex Toucan <alextoucan.at@gmail.com>
2024-07-24 13:12:13 -0500

    Testing console log.

---
+++ public/static/message.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  console.log('%cHello there fellow traveler!%c\nNothing you can do on this website that\'ll get you hacked, so do whatever you want here in the console. Also, your cookies don\'t personally identify you, so you can also mess with those.',
+              'font-weight: bold; font-size: 24px; color: green;', // Style for header
+              'font-size: 16px; color: black;'                     // Style for body text
+             );
+});
