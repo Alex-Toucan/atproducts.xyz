032b3a9d2bd736d98d3d9debcfc3e2505b25cab1
Alex Toucan <alextoucan.at@gmail.com>
2024-07-24 13:18:15 -0500

    reword

--- public/static/message.js
+++ public/static/message.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  console.log('%cHello there fellow traveler!%c\nFeel free to explore and experiment with the console. Rest assured, nothing on this site can compromise your security, and your cookies do not contain personally identifiable information. Feel free to play around with the console if you want to.',
+  console.log('%cHello there fellow traveler!%c\nFeel free to explore and experiment with the console. Rest assured, nothing on this site can compromise your security, and your cookies do not contain personally identifiable information.',
               'font-weight: bold; font-size: 24px; color: green;', // Style for header
               'font-size: 16px; color: white;'                     // Style for body text
              );
