39802e45c0623c0055860b9c03ff6d21b8ef7a35
Alex Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2025-04-25 09:42:04 -0500

    Revert "Add ezpkgm as a Pynx Project. (#506)" (#508)
    This reverts commit 3f5128119b2dc7b83dab493a87e966b1ea4bf474.

---
+++ public/media/files/Windows_Screen_Grabber_V2.py
@@ -0,0 +1,10 @@
+'''
+pip install pyscreeze
+
+See: https://pypi.org/project/PyScreeze/
+for more features.
+'''
+
+import pyscreeze
+
+pyscreeze.screenshot("screen.png")
---
+++ public/media/files/pwned.py
@@ -0,0 +1,23 @@
+import hashlib, requests, os
+from getpass import getpass
+
+
+def pwn(password, hashed_password):
+    r = requests.get(f'https://api.pwnedpasswords.com/range/{password}', verify=True)
+    pwnd = r.text.split("\r\n")
+    for data in pwnd:
+        if data[0:35] == hashed_password.upper():
+            return(input(f'\nOh no - pwned!\nThis password has been seen {data[36:]} times before\n'))
+
+    return(input("\nGood news - no pwnage found!\n"))
+
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # get password
+    password = getpass("\n';--have i been pwned?: ")
+    encode = hashlib.pbkdf2_hmac(password.encode('utf-8'))
+    salt = os.urandom(16)
+    hashed_password = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
+    pwnd_password = hashlib.pbkdf2_hmac(password.encode('utf-8')).hexdigest()[0:5]
+    
+    pwn(pwnd_password, hashed_password.hex()[5:])
