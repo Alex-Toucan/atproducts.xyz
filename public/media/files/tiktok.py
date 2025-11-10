80030734b9d2143728f5d936556aa4de9e0eca88
Alex Toucan <alextoucan.at@gmail.com>
2025-11-09 21:17:16 -0600

    Remove all files from /media/files

--- public/media/files/tiktok.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-username = input("Enter TikTok username: ")
-response = requests.get(f"https://www.tiktok.com/@{username}")
-if response.status_code == 200:
-    print("+")
-else:
-    print("-")
