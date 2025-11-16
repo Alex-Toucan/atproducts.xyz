33760f908e0c031b372c7a25c27d3ba634a6ad31
Alex Toucan <alextoucan.at@gmail.com>
2025-11-16 14:35:36 -0600

    Add more games

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+/* import compress from "astro-compress"; */
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
-  ],
-  experimental: {
-    csp: {
-      styleDirective: {
-        resources: [
-          "'self'",
-          "'unsafe-inline'",
-          "https://cdn.jsdelivr.net"
-        ]
-      },
-      scriptDirective: {
-        resources: [
-          "'self'",
-          "'unsafe-inline'",
-          "'unsafe-hashes'",
-          "https://cdn.jsdelivr.net"
-        ]
-      },
-      directives: [
-        "default-src 'self'",
-        "frame-src 'self' https://app.netlify.com/ https://codepen.io https://javaspence.github.io/",
-        "form-action 'self'",
-        "base-uri 'self'",
-        "manifest-src 'self'",
-        "font-src 'self' data: https://cdn.jsdelivr.net",
-        "object-src 'self'",
-        "img-src 'self' data:"
-      ]
-    }
-  }
-});
+    react()/*, 
+    compress({
+      css: false,
+      html: true,
+      img: true,
+      js: true,
+      image: true,
+      svg: true,
+    })
+    */
+  ]
+})
