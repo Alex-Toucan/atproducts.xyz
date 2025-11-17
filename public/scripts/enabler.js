6f8d55aec7bc944558d55a86e96cfd08b86e10dc
Alex Toucan <alextoucan.at@gmail.com>
2025-11-16 22:01:42 -0600

    alot of work

---
+++ public/scripts/enabler.js
@@ -0,0 +1,9 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const popover = new bootstrap.Popover('.popover-dismiss', {
+      trigger: 'focus'
+})
