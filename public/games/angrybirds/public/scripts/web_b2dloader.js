6412f54fee8fd36a9295fc92f9fc8671d63b2357
Alex-Toucan <alextoucan.at@gmail.com>
2025-11-25 11:38:33 -0600

    Change to swf

--- public/games/angrybirds/public/scripts/web_b2dloader.js
+++
@@ -1,8 +0,0 @@
-// vite has a bug where it doesn't load the module correctly
-// so we need to load it manually
-
-//jsgamelauncher doesn't need to do this hack
-
-import Box2DFactory from './Box2D.deluxe.mjs';
-
-window.Box2DFactory = Box2DFactory;
--- public/games/angrybirds/src/index_browser.js
+++
@@ -1,4 +0,0 @@
-// launching from here in browser because vite has a bug where it doesn't load the module correctly
-import { start } from './start.js';
-
-start(window.Box2DFactory);
