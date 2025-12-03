0a54e7a7b9c16d8d416557cfd623b9c1516edc93
Alex Toucan <alextoucan.at@gmail.com>
2025-12-02 19:30:08 -0600

    Fix errors (pt 2)

---
+++ public/games/poker/js/cards.js
@@ -0,0 +1,174 @@
+let cards = [
+    /*START SPADES */
+    {
+        title: "ace-spades",
+        value: 11
+    },
+    {
+        title: "two-spades",
+        value: 2
+    },
+    {
+        title: "three-spades",
+        value: 3
+    }, {
+        title: "four-spades",
+        value: 4
+    }, {
+        title: "five-spades",
+        value: 5
+    }, {
+        title: "six-spades",
+        value: 6
+    }, {
+        title: "seven-spades",
+        value: 7
+    }, {
+        title: "eight-spades",
+        value: 8
+    }, {
+        title: "nine-spades",
+        value: 9
+    }, {
+        title: "ten-spades",
+        value: 10
+    }, {
+        title: "jack-spades",
+        value: 10
+    }, {
+        title: "queen-spades",
+        value: 10
+    }, {
+        title: "king-spades",
+        value: 10
+    },
+    /*START HEARTS */
+    {
+        title: "ace-hearts",
+        value: 11
+    },
+    {
+        title: "two-hearts",
+        value: 2
+    },
+    {
+        title: "three-hearts",
+        value: 3
+    }, {
+        title: "four-hearts",
+        value: 4
+    }, {
+        title: "five-hearts",
+        value: 5
+    }, {
+        title: "six-hearts",
+        value: 6
+    }, {
+        title: "seven-hearts",
+        value: 7
+    }, {
+        title: "eight-hearts",
+        value: 8
+    }, {
+        title: "nine-hearts",
+        value: 9
+    }, {
+        title: "ten-hearts",
+        value: 10
+    }, {
+        title: "jack-hearts",
+        value: 10
+    }, {
+        title: "queen-hearts",
+        value: 10
+    }, {
+        title: "king-hearts",
+        value: 10
+    },
+    /*START DIAMONDS */
+    {
+        title: "ace-diamonds",
+        value: 11
+    },
+    {
+        title: "two-diamonds",
+        value: 2
+    },
+    {
+        title: "three-diamonds",
+        value: 3
+    }, {
+        title: "four-diamonds",
+        value: 4
+    }, {
+        title: "five-diamonds",
+        value: 5
+    }, {
+        title: "six-diamonds",
+        value: 6
+    }, {
+        title: "seven-diamonds",
+        value: 7
+    }, {
+        title: "eight-diamonds",
+        value: 8
+    }, {
+        title: "nine-diamonds",
+        value: 9
+    }, {
+        title: "ten-diamonds",
+        value: 10
+    }, {
+        title: "jack-diamonds",
+        value: 10
+    }, {
+        title: "queen-diamonds",
+        value: 10
+    }, {
+        title: "king-diamonds",
+        value: 10
+    },
+    /*START CLUBS */
+    {
+        title: "ace-clubs",
+        value: 11
+    },
+    {
+        title: "two-clubs",
+        value: 2
+    },
+    {
+        title: "three-clubs",
+        value: 3
+    }, {
+        title: "four-clubs",
+        value: 4
+    }, {
+        title: "five-clubs",
+        value: 5
+    }, {
+        title: "six-clubs",
+        value: 6
+    }, {
+        title: "seven-clubs",
+        value: 7
+    }, {
+        title: "eight-clubs",
+        value: 8
+    }, {
+        title: "nine-clubs",
+        value: 9
+    }, {
+        title: "ten-clubs",
+        value: 10
+    }, {
+        title: "jack-clubs",
+        value: 10
+    }, {
+        title: "queen-clubs",
+        value: 10
+    }, {
+        title: "king-clubs",
+        value: 10
+    }
+];
