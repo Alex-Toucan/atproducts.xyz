845e9ce2d597ebe43da7a6c0a66b0c60240a1afa
Alex Toucan <alextoucan.at@gmail.com>
2025-12-15 19:39:37 -0600

    Add theme switcher + info button

--- public/static/darkmode.js
+++ public/static/darkmode.js
@@ -1,9 +1,3 @@
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2024 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
-
 (() => {
   'use strict'
 
@@ -13,13 +7,15 @@
     if (storedTheme) {
       return storedTheme
     }
-
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
 
-  const setTheme = function (theme) {
+  const setTheme = (theme) => {
     if (theme === 'auto') {
-      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+      document.documentElement.setAttribute(
+        'data-bs-theme',
+        window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+      )
     } else {
       document.documentElement.setAttribute('data-bs-theme', theme)
     }
@@ -29,14 +25,14 @@
 
   const showActiveTheme = (theme, focus = false) => {
     const themeSwitcher = document.querySelector('#bd-theme')
-
-    if (!themeSwitcher) {
-      return
-    }
+    if (!themeSwitcher) return
 
     const themeSwitcherText = document.querySelector('#bd-theme-text')
     const activeThemeIcon = document.querySelector('.theme-icon-active i')
     const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+
+    if (!btnToActive) return // prevent null errors
+
     const svgOfActiveBtn = btnToActive.querySelector('i').getAttribute('class')
 
     for (const element of document.querySelectorAll('[data-bs-theme-value]')) {
@@ -47,17 +43,17 @@
     btnToActive.classList.add('active')
     btnToActive.setAttribute('aria-pressed', 'true')
     activeThemeIcon.setAttribute('class', svgOfActiveBtn)
+
     const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
     themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
 
-    if (focus) {
-      themeSwitcher.focus()
-    }
+    if (focus) themeSwitcher.focus()
   }
 
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    if (storedTheme !== 'light' || storedTheme !== 'dark') {
+    if (storedTheme !== 'light' && storedTheme !== 'dark') {
       setTheme(getPreferredTheme())
+      showActiveTheme(getPreferredTheme())
     }
   })
 
@@ -72,5 +68,5 @@
         showActiveTheme(theme, true)
       })
     }
-  });
+  })
 })()
