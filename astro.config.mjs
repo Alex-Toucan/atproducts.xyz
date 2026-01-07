21d9c7c012a8c9875f8265680d2af5925f8a9872
Alex Toucan <alextoucan.at@gmail.com>
2026-01-07 06:53:42 -0600

    Replace CSP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,40 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
-/* import compress from "astro-compress"; */
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
-    react()/*, 
-    compress({
-      css: false,
-      html: true,
-      img: true,
-      js: true,
-      image: true,
-      svg: true,
-    })
-    */
+    react(),
   ],
+  adapter: netlify(),
   output: 'server',
-  adapter: netlify()
-})
+  experimental: {
+    csp: {
+      styleDirective: {
+        resources: [
+          "'self'",
+          "'unsafe-inline'",
+          "https://cdn.jsdelivr.net"
+        ]
+      },
+      scriptDirective: {
+        resources: [
+          "'self'",
+          "'unsafe-inline'",
+          "'unsafe-hashes'",
+          "https://cdn.jsdelivr.net"
+        ]
+      },
+      directives: [
+        "default-src 'self'",
+        "frame-src 'self' https://app.netlify.com/ https://codepen.io",
+        "form-action 'self'",
+        "base-uri 'self'",
+        "manifest-src 'self'",
+        "font-src 'self' data: https://cdn.jsdelivr.net",
+        "object-src 'self'",
+        "img-src 'self' data:"
+      ]
+    }
+  }
+});
