ec6ed93fef4e0579280ae033ff7049780a5c1d24
Alex-Toucan <alextoucan.at@gmail.com>
2026-02-04 19:59:43 -0600

    Remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
-/* import compress from "astro-compress"; */
 
 export default defineConfig({
   vite: {
@@ -12,16 +11,7 @@ export default defineConfig({
     }
   },
   integrations: [
-    react()/*, 
-    compress({
-      css: false,
-      html: true,
-      img: true,
-      js: true,
-      image: true,
-      svg: true,
-    })
-    */
+    react()
   ],
   adapter: netlify(),
   experimental: {
