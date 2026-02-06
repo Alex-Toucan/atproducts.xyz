e24bc184b484cb22a3a91fbbd54f4becdde60d9f
Alex-Toucan <alextoucan.at@gmail.com>
2026-02-05 22:21:26 -0600

    Add "Tap Tap Shots"

---
+++ public/games/taptapshots/scripts/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Eqkyads_Event2_Act3(runtime, localVars)
+	{
+		await crazysdk.displayAd('midgame');
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
