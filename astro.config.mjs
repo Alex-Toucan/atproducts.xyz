a67ea7077438b0e0a6ae24c78d26fded89f69243
Alex-Toucan <alextoucan.at@gmail.com>
2026-03-08 21:05:47 -0500

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        jquery: '/node_modules/jquery'
+      }
+    }
+  },
   integrations: [
     react(),
   ],
-  adapter: netlify(),
-  output: 'server',
-  experimental: {
-    csp: {
-      styleDirective: {
-        resources: [
-          "'self'",
-          "'unsafe-inline'",
-          "https://cdn.jsdelivr.net"
-        ]
-      },
-      scriptDirective: {
-        resources: [
-          "'self'",
-          "'unsafe-inline'",
-          "'unsafe-hashes'",
-          "https://cdn.jsdelivr.net"
-        ]
-      },
-      directives: [
-        "default-src 'self'",
-        "frame-src 'self' https://app.netlify.com/ https://codepen.io",
-        "form-action 'self'",
-        "base-uri 'self'",
-        "manifest-src 'self'",
-        "font-src 'self' data: https://cdn.jsdelivr.net",
-        "object-src 'self'",
-        "img-src 'self' data:"
-      ]
-    }
-  }
 });
