051a2ba178b8006661583c2c911bc5c9848fd384
Alex-Toucan <alextoucan.at@gmail.com>
2026-04-09 22:05:52 -0500

    Fix color toggle switch

---
+++ public/scripts/navbar.js
@@ -0,0 +1,14 @@
+function adjustDropdowns() {
+    const isMobile = window.innerWidth < 992;
+
+    document.querySelectorAll("#bd-theme").forEach(d => {
+        if (isMobile) {
+            d.setAttribute("data-bs-placement", "top-end");
+          } else {
+            d.setAttribute("data-bs-placement", "bottom-start");
+        }
+    });
+}
+
+window.addEventListener("resize", adjustDropdowns);
+adjustDropdowns();
