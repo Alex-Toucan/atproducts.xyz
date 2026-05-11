146f6860a4a82a97b731c1e3a15ec2f50ac61bae
Alex-Toucan <alextoucan.at@gmail.com>
2026-05-10 22:26:19 -0500

    Day 1 Work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,38 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import react from '@astrojs/react';
 /* import compress from "astro-compress"; */
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      name: "Audiowide",
+      cssVariable: "--font-audiowide",
+      provider: fontProviders.google(),
+      styles: ["normal"]
+    },
+    {
+      provider: fontProviders.local(),
+      name: "CustomIcons",
+      cssVariable: "--font-customicons", 
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/CustomFonts.woff"],
+            display: "block"
+          },
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/CustomFonts.woff2"],
+            display: "block"
+          }
+        ]
+      }
+    },
+  ],
   integrations: [
     react()/*, 
     compress({
