432ad5caee252c979f8d6ff7f0cb460fd14da6c2
Alex-Toucan <alextoucan.at@gmail.com>
2026-05-20 21:12:06 -0500

    Remove index.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,5 @@
 import { defineConfig } from 'astro/config';
-/* import compress from "astro-compress"; */
 
 export default defineConfig({
-  csp: {
-    styleDirective: {
-      resources: [
-        "'self'",
-        "'unsafe-inline'",
-        "https://cdn.jsdelivr.net"
-      ]
-    },
-    scriptDirective: {
-      resources: [
-        "'self'",
-        "'unsafe-inline'",
-        "'unsafe-hashes'",
-        "https://cdn.jsdelivr.net"
-      ]
-    },
-    directives: [
-      "default-src 'none'",
-      "frame-src 'self' https://app.netlify.com/ https://codepen.io",
-      "form-action 'self'",
-      "base-uri 'self'",
-      "manifest-src 'self'",
-      "font-src 'self' data: https://cdn.jsdelivr.net",
-      "object-src 'self'",
-      "img-src 'self' data:"
-    ]
-  }
+
 });
