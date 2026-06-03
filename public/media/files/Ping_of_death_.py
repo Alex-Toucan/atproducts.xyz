2e31bdd298649b35c82934c49a518feb4aa4cd7b
Alex-Toucan <alextoucan.at@gmail.com>
2026-06-02 19:30:48 -0500

    Remove unneeded public files

--- public/media/files/Ping_of_death_.py
+++
@@ -1,4 +0,0 @@
-import os
-ip = input("IP: ")
-while True:
-os.system("ping {ip} -l 65500 -w 1 -n 1")
--- public/media/files/Service1.cs
+++
@@ -1,3 +0,0 @@
-public class Service1 : IService1 {
-  public void Method1() { // Implementation }
-}
