5ca219b47a6b60c9edd9c2f38e05c995832fca9f
Alex-Toucan <alextoucan.at@gmail.com>
2026-06-06 00:00:12 -0500

    Add Back to Top button + JQuery

---
+++ public/static/scroll.js
@@ -0,0 +1,29 @@
+(function ($) {
+  "use strict";
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+          if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+              $('.back-to-top').css('display', 'block');
+          } else {
+              $('.back-to-top').fadeIn('slow');
+              $('.back-to-top').css('display', 'flex');
+          }
+      } else {
+          if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+              $('.back-to-top').css('display', 'none');
+          } else {
+              $('.back-to-top').fadeOut('slow');
+          }
+      }
+  });
+
+  $('.back-to-top').click(function () {
+      if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+          $('html, body').scrollTop(0);
+      } else {
+          $('html, body').animate({scrollTop: 0}, 500, 'easeInOutExpo');
+      }
+      return false;
+  });
+
+})(jQuery);
