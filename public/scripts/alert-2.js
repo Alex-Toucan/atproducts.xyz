d889adc603d13acdfcfdd1f552efda3fc482246b
Alex-Toucan <alextoucan.at@gmail.com>
2026-06-10 12:44:19 -0500

    Enable alert

--- public/scripts/alert-2.js
+++ public/scripts/alert-2.js
@@ -1,11 +1,11 @@
 // --- Config --- //
 var pureAlert2Type = "alert-dark"; // Color
-var pureAlert2Title = "A NEW STATEMENT REGARDING DISCORD SERVERS:"; // Title
-var pureAlert2Icon = 'bi-discord'; // Icon
-var pureAlert2Desc = "Due to recent world events, us as a company has released an statement regarding how we protect minors on our Discord Servers."; // Description
-var pureAlert2Link = 'https://other.atproducts.xyz/minors'; // Link
-var pureAlert2LinkDesc = 'Read the statement.'; // Link text
-var pureAlert2Enabled = false; // Switch to enable/disable alert
+var pureAlert2Title = "GITHUB TRANSFER NOTICE:"; // Title
+var pureAlert2Icon = 'bi-github'; // Icon
+var pureAlert2Desc = "On June 17th, the GitHub repository: <a href='https://github.com/Alex-Toucan/atproducts.xyz' target='_blank'>Alex-Toucan/atproducts.xyz</a>, will be moved from the <a href='https://github.com/Alex-Toucan' target='_blank'>Alex-Toucan GitHub account</a> to the <a href='https://github.com/ATProductsLLC' target='_blank'>ATProductsLLC GitHub account</a>. As a result, Maintenance Mode will be enabled for approximately 5:30 to 6:30 PM CDT on June 17th. We appreciate your patience as we improve our site."; // Description
+var pureAlert2Link = 'https://x.com/ATProductsLLC/status/2061307838790521052'; // Link
+var pureAlert2LinkDesc = 'Read the announcement.'; // Link text
+var pureAlert2Enabled = true; // Switch to enable/disable alert
 // ---        --- //
 
 function setAlert2Cookie(name, value, days) {
